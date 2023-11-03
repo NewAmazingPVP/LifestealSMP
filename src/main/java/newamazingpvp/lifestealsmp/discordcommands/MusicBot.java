290ba8c524542df93bfa59c8e582d01b81e5a526
NewAmazingPVP <gamingofpat@gmail.com>
2023-11-02 22:38:21 -0400

    Update MusicBot.java

--- src/main/java/newamazingpvp/lifestealsmp/discordcommands/MusicBot.java
+++ src/main/java/newamazingpvp/lifestealsmp/discordcommands/MusicBot.java
@@ -6,12 +6,12 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class MusicBot extends ListenerAdapter {
 
 
-    @Override
+    /*@Override
     public void onMessageReceived(MessageReceivedEvent event) {
         String[] command = event.getMessage().getContentRaw().split(" ", 2);
         if ("!play".equals(command[0]) && command.length > 1) {
             event.getMessage()
         }
-    }
+    }*/
 }
 
