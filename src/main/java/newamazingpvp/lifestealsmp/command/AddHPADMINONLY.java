cc7711d347d9a6bdf8e163250a01fb2cac3ae87b
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-11-21 17:20:25 -0500

    1

---
+++ src/main/java/newamazingpvp/lifestealsmp/command/AddHPADMINONLY.java
@@ -0,0 +1,24 @@
+package newamazingpvp.lifestealsmp.command;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class AddHPADMINONLY implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "This command can only be executed by players.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+        player.setMaxHealth(player.getMaxHealth() + 2);
+
+        return true;
+
+    }
+}
