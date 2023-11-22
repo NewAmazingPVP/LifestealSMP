7a06b8dba95c6c6a00960a74e4a24f5852231e8e
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-11-22 12:44:27 -0500

    1

--- src/main/java/newamazingpvp/lifestealsmp/command/GiveMontuHelmetADMINONLY.java
+++ src/main/java/newamazingpvp/lifestealsmp/command/GiveMontuHelmetADMINONLY.java
@@ -7,21 +7,28 @@ import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.profile.PlayerProfile;
 import org.bukkit.profile.PlayerTextures;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+
+import static newamazingpvp.lifestealsmp.game.CustomRecipe.montuHelm;
 
 public class GiveMontuHelmetADMINONLY implements CommandExecutor {
 
     private static final UUID RANDOM_UUID = UUID.fromString("92864445-51c5-4c3b-9039-517c9927d1b4"); // We reuse the same "random" UUID all the time
 
-    private static PlayerProfile getProfile(String url) {
+    public static PlayerProfile getProfile(String url) {
         PlayerProfile profile = Bukkit.createPlayerProfile(RANDOM_UUID); // Get a new player profile
         PlayerTextures textures = profile.getTextures();
         URL urlObject;
@@ -38,12 +45,8 @@ public class GiveMontuHelmetADMINONLY implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
-        PlayerProfile profile = getProfile("https://textures.minecraft.net/texture/3070fad4da430fc24141542918ca184803d7f50a8a8e263d0331e3fd62029336");
-        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
-        SkullMeta meta = (SkullMeta) head.getItemMeta();
-        meta.setOwnerProfile(profile);
-        head.setItemMeta(meta);
-        player.getInventory().addItem(head);
+
+        player.getInventory().addItem(montuHelm());
 
         player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1.0f, 2.0f);
         return true;
