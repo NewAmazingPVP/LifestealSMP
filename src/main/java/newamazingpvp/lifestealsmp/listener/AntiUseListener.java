4c711f1a6922dbe5faef2d3e2830582bfac244c5
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-11-22 10:28:12 -0500

    Update AntiUseListener.java

--- src/main/java/newamazingpvp/lifestealsmp/listener/AntiUseListener.java
+++ src/main/java/newamazingpvp/lifestealsmp/listener/AntiUseListener.java
@@ -16,7 +16,7 @@ public class AntiUseListener implements Listener {
 
         if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             ItemStack item = event.getItem();
-            if (item != null && item.getType() == Material.STICK && item.hasItemMeta() && item.getItemMeta().hasDisplayName() && item.getItemMeta().getDisplayName().equals(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Severed Mob Heart")) {
+            if (item != null && item.getType() == Material.BEETROOT && item.hasItemMeta() && item.getItemMeta().hasDisplayName() && item.getItemMeta().getDisplayName().equals(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Severed Mob Heart")) {
 
                 event.setCancelled(true);
 
