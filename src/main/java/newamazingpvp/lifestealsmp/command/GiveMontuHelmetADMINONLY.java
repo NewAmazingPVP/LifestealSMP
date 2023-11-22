5d293b06a27c23164e7d22cac531deb174c4ca91
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-11-22 12:21:19 -0500

    Update GiveMontuHelmetADMINONLY.java

--- src/main/java/newamazingpvp/lifestealsmp/command/GiveMontuHelmetADMINONLY.java
+++ src/main/java/newamazingpvp/lifestealsmp/command/GiveMontuHelmetADMINONLY.java
@@ -10,29 +10,43 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.profile.PlayerProfile;
+import org.bukkit.profile.PlayerTextures;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.UUID;
 
 public class GiveMontuHelmetADMINONLY implements CommandExecutor {
 
+    private static final UUID RANDOM_UUID = UUID.fromString("92864445-51c5-4c3b-9039-517c9927d1b4"); // We reuse the same "random" UUID all the time
+
+    private static PlayerProfile getProfile(String url) {
+        PlayerProfile profile = Bukkit.createPlayerProfile(RANDOM_UUID); // Get a new player profile
+        PlayerTextures textures = profile.getTextures();
+        URL urlObject;
+        try {
+            urlObject = new URL(url); // The URL to the skin
+        } catch (MalformedURLException exception) {
+            throw new RuntimeException("Invalid URL", exception);
+        }
+        textures.setSkin(urlObject); // Set the skin of the player profile to the URL
+        profile.setTextures(textures); // Set the textures back to the profile
+        return profile;
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            ItemStack MintyHelm = new ItemStack(Material.PLAYER_HEAD);
-            SkullMeta meta = (SkullMeta) MintyHelm.getItemMeta();
+        Player player = (Player) sender;
+        PlayerProfile profile = getProfile("https://textures.minecraft.net/texture/3070fad4da430fc24141542918ca184803d7f50a8a8e263d0331e3fd62029336");
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta meta = (SkullMeta) head.getItemMeta();
+        meta.setOwnerProfile(profile); // Set the owning player of the head to the player profile
+        head.setItemMeta(meta);
+        player.getInventory().addItem(head);
 
-            meta.setOwningPlayer(Bukkit.getOfflinePlayer(UUID.fromString("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzA3MGZhZDRkYTQzMGZjMjQxNDE1NDI5MThjYTE4NDgwM2Q3ZjUwYThhOGUyNjNkMDMzMWUzZmQ2MjAyOTMzNiJ9fX0=")));
-            meta.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Montu Helmet");
-
-            MintyHelm.setItemMeta(meta);
-            player.getInventory().addItem(MintyHelm);
-
-            player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1.0f, 2.0f);
-            player.sendMessage(ChatColor.GREEN + "Giving Item!");
-            return true;
-        }
-
-        return false;
+        player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1.0f, 2.0f);
+        return true;
     }
+
 }
