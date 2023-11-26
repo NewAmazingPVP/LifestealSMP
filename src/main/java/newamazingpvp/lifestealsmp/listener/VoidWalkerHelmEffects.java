7e2b0e1ffc48575ddc0d8d26a43705dd4f19126e
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-11-26 17:51:57 -0500

    1

---
+++ src/main/java/newamazingpvp/lifestealsmp/listener/VoidWalkerHelmEffects.java
@@ -0,0 +1,44 @@
+package newamazingpvp.lifestealsmp.listener;
+
+import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.EventListener;
+
+public class VoidWalkerHelmEffects implements Listener {
+
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageByEntityEvent e) {
+
+        Entity damager = e.getDamager();
+        Entity damageTaker = e.getEntity();
+
+        if (e.getEntity() instanceof Player) {
+            Player player = (Player) e.getEntity();
+            ItemStack helmet = player.getInventory().getHelmet();
+            if (helmet != null) {
+                ItemMeta meta = helmet.getItemMeta();
+                if (meta.hasDisplayName() && meta.getDisplayName().equals(ChatColor.LIGHT_PURPLE + "" + ChatColor.MAGIC + "LL " + ChatColor.DARK_RED + "" + ChatColor.BOLD + "Void Walker Helmet" + ChatColor.LIGHT_PURPLE + "" + ChatColor.MAGIC + " LL")) {
+                    if (damageTaker instanceof Player) {
+
+                        player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 20 * 60, 1));
+                        player.sendMessage("Detected");
+
+                    }
+                }
+            }
+        }
+    }
+}
+
