31f5e5064ee5f778ebedbbcc578dd997d753662d
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-11-29 12:55:03 -0500

    chatfilter

--- src/main/java/newamazingpvp/lifestealsmp/listener/ChatFilter.java
+++ src/main/java/newamazingpvp/lifestealsmp/listener/ChatFilter.java
@@ -7,31 +7,44 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class ChatFilter implements Listener {
+    public static final ArrayList<String> blacklistWords = new ArrayList<>();
+
+    public static void initializeBlacklist() {
+        try {
+            Scanner input = new Scanner(new File("blacklist.csv"));
+            while (input.hasNextLine()) {
+                String[] temp = input.nextLine().split(",");
+                List<String> words = Arrays.asList(temp);
+                words = words.stream()
+                        .map(word -> word.replaceAll("[,\\s]", ""))
+                        .collect(Collectors.toList());
+
+                blacklistWords.addAll(words);
+            }
+            input.close();
+        } catch (Exception e) {
+            System.out.println("Error reading or parsing blacklist.csv");
+        }
+    }
+
 
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         String message = event.getMessage();
         Player player = event.getPlayer();
-        if (message.contains("dick")) {
+        if (blacklistWords.contains(message)) {
             event.setCancelled(true);
             player.sendMessage(ChatColor.RED + "You can not say inappropriate / offensive words in chat!");
             player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1.0f, 2.0f);
         }
-        if (message.contains("kys")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You can not say inappropriate / offensive words in chat!");
-            player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1.0f, 2.0f);
-        }
-        if (message.contains("keep yourself safe")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You can not say inappropriate / offensive words in chat!");
-            player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1.0f, 2.0f);
-        }
-        if (message.contains("fag")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You can not say inappropriate / offensive words in chat!");
-            player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1.0f, 2.0f);
-        }
+
     }
 }
