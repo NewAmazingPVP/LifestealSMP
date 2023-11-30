99201b2764db8a798ac538fe6b7c4d6367fd7c1f
adamzecoder <adam.kronenberg13@gmail.com>
2023-11-29 22:03:32 -0500

    fat

---
+++ src/main/java/newamazingpvp/lifestealsmp/listener/Test_Reflect_Shield.java
@@ -0,0 +1,25 @@
+package newamazingpvp.lifestealsmp.listener;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class Test_Reflect_Shield implements Listener {
+
+    public void onPlayerUseShield(PlayerInteractEvent event) {
+
+        Player player = event.getPlayer();
+
+        ItemStack item = player.getInventory().getItemInMainHand();
+        if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (item.getType() == Material.FEATHER && item.hasItemMeta() && item.getItemMeta().hasDisplayName()
+                    && item.getItemMeta().getDisplayName().equals(ChatColor.GREEN + "Reflect Shield")) {
+
+            }
+        }
+    }
+}
