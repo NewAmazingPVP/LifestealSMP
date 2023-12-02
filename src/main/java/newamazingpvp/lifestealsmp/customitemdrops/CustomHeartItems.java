00c524e4246ae4139d598ec0c3cdf65a177c5809
NewAmazingPVP <gamingofpat@gmail.com>
2023-12-02 18:29:09 -0500

    w

--- src/main/java/newamazingpvp/lifestealsmp/customitemdrops/CustomHeartItems.java
+++ src/main/java/newamazingpvp/lifestealsmp/customitemdrops/CustomHeartItems.java
@@ -52,7 +52,7 @@ public class CustomHeartItems implements Listener {
 
         if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
             if (item.getType() == Material.RED_DYE && item.hasItemMeta() && item.getItemMeta().hasDisplayName() && item.getItemMeta().getDisplayName().equals(ChatColor.LIGHT_PURPLE + "" + ChatColor.MAGIC + "LL" + ChatColor.DARK_RED + ChatColor.BOLD + "Extra Heart" + ChatColor.LIGHT_PURPLE + ChatColor.MAGIC + "LL")) {
-                if (e.getPlayer().getMaxHealth() >= 40) {
+                if (e.getPlayer().getMaxHealth() >= 38) {
 
                     player.sendMessage(ChatColor.RED + "You have the max hearts allowed! (20)");
                     e.setCancelled(true);
