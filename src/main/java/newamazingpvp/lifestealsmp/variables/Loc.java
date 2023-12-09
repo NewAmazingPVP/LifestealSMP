c692a7dfebff3b16a1b515bd82a3046afff9fa55
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-12-08 22:39:04 -0500

    shop

--- src/main/java/newamazingpvp/lifestealsmp/variables/Loc.java
+++ src/main/java/newamazingpvp/lifestealsmp/variables/Loc.java
@@ -7,4 +7,5 @@ public class Loc {
     public static Location endFightSpawn = new Location(Bukkit.getWorld("world_the_end"), 25.0, 80.0, 25.0);
     public static Location endSpawn = new Location(Bukkit.getWorld("world_the_end"), 0, 60.0, 0);
     public static Location endPortalCenter = new Location(Bukkit.getWorld("world_the_end"), 0, 70.0, 0);
+    public static Location lobby = new Location(Bukkit.getWorld("world"), 0, 300.0, 0);
 }
