a81da14d0ad4017c9a4e4205489f810ac60dc369
NewAmazingPVP <gamingofpat@gmail.com>
2023-12-17 14:17:08 -0500

    beacon invis fix

--- src/main/java/newamazingpvp/lifestealsmp/listener/BeaconInvis.java
+++ src/main/java/newamazingpvp/lifestealsmp/listener/BeaconInvis.java
@@ -1,8 +1,10 @@
 package newamazingpvp.lifestealsmp.listener;
 
+import com.destroystokyo.paper.event.block.BeaconEffectEvent;
+import io.papermc.paper.event.block.BeaconDeactivatedEvent;
 import org.bukkit.Location;
 import org.bukkit.block.Beacon;
-import org.bukkit.entity.Entity;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,45 +17,20 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import static newamazingpvp.lifestealsmp.LifestealSMP.lifestealSmp;
+import static org.bukkit.Bukkit.getServer;
+
 public class BeaconInvis implements Listener {
-
-    private final Map<UUID, Beacon> playersInBeaconRange = new HashMap<>();
-
     @EventHandler
-    public void onPlayerMove(PlayerMoveEvent event) {
-        Player player = event.getPlayer();
-        Location playerLocation = player.getLocation();
+    public void onPlayerBeaconEffect(BeaconEffectEvent event) {
+        Block block = event.getBlock();
 
-        Beacon beacon = getFullyPoweredBeaconInRange(playerLocation);
-        if (beacon != null) {
-            if (!playersInBeaconRange.containsKey(player.getUniqueId())) {
-                player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 0, true, false));
-                playersInBeaconRange.put(player.getUniqueId(), beacon);
-            }
-        } else {
-            if (playersInBeaconRange.containsKey(player.getUniqueId())) {
-                player.removePotionEffect(PotionEffectType.INVISIBILITY);
-                playersInBeaconRange.remove(player.getUniqueId());
+        if (block.getState() instanceof Beacon) {
+            Beacon beacon = (Beacon) block.getState();
+
+            if (beacon.getTier() == 4) {
+                event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 17 * 20, 0, true, false));
             }
         }
-    }
-
-    @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent event) {
-        playersInBeaconRange.remove(event.getPlayer().getUniqueId());
-    }
-
-    private Beacon getFullyPoweredBeaconInRange(Location playerLocation) {
-        double beaconRange = 50;
-
-        for (Entity entity : playerLocation.getWorld().getNearbyEntities(playerLocation, beaconRange, 384, beaconRange)) {
-            if (entity instanceof Beacon) {
-                Beacon beacon = (Beacon) entity;
-                if (playerLocation.distanceSquared(beacon.getLocation()) <= beaconRange * beaconRange && beacon.getTier() == 4) {
-                    return beacon;
-                }
-            }
-        }
-        return null;
     }
 }
