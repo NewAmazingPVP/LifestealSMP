7a1a7cbb42ad0ed53b632e1309992103935473cf
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-04-04 19:56:36 -0400

    less spawn prot and trying to disable egg from being place anywhere but its not working and nevers works
    less spawn prot and trying to disable egg from being place anywhere but its not working and nevers works

---
+++ src/main/java/newamazingpvp/lifestealsmp/blacklistener/DisableEnderDragonEgg.java
@@ -0,0 +1,51 @@
+package newamazingpvp.lifestealsmp.blacklistener;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.*;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class DisableEnderDragonEgg implements Listener {
+
+    private final String message = ChatColor.RED + "You can't take the egg off your inventory";
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY) {
+            if (event.getCurrentItem() != null &&
+                    event.getCurrentItem().getType() == Material.DRAGON_EGG) {
+                if (event.getClickedInventory().getType()!= InventoryType.PLAYER) {
+                    event.setCancelled(true);
+                    event.getWhoClicked().sendMessage(message);
+                }
+                //event.getWhoClicked().closeInventory();
+            }
+        }
+    }
+
+    @EventHandler
+    public void onInventoryDrag(InventoryDragEvent event) {
+        if (event.getOldCursor().getType() == Material.DRAGON_EGG) {
+            event.setCancelled(true);
+            event.getWhoClicked().sendMessage(message);
+            //event.getWhoClicked().closeInventory();
+        }
+    }
+
+    @EventHandler
+    public void onInventoryDrag(InventoryMoveItemEvent event) {
+        if (event.getItem().getType().equals(Material.DRAGON_EGG) && !event.getDestination().getType().equals(InventoryType.PLAYER)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryDrag(InventoryPickupItemEvent event) {
+        if (event.getItem().getItemStack().getType().equals(Material.DRAGON_EGG) && !event.getInventory().getType().equals(InventoryType.PLAYER)) {
+            event.setCancelled(true);
+        }
+    }
+
+}
