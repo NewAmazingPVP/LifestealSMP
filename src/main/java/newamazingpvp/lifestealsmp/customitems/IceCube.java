b08bf258afd77922e0f54bdb54e36f979a432c94
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-04-07 19:44:53 -0400

    -_-

--- src/main/java/newamazingpvp/lifestealsmp/customitems/IceCube.java
+++ src/main/java/newamazingpvp/lifestealsmp/customitems/IceCube.java
@@ -10,16 +10,72 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static newamazingpvp.lifestealsmp.LifestealSMP.lifestealSmp;
 
 public class IceCube implements Listener {
 
     //not the fucking rapper
+
+    public static ItemStack FrozenIceCP () {
+        ItemStack chestplate = new ItemStack(Material.LEATHER_CHESTPLATE);
+        LeatherArmorMeta chestplateMeta = (LeatherArmorMeta) chestplate.getItemMeta();
+        chestplateMeta.setDisplayName(ChatColor.AQUA + "" + ChatColor.BOLD + "Ice");
+        chestplateMeta.setColor(Color.fromRGB(255, 85, 0));
+        chestplateMeta.setUnbreakable(true);
+        List<String> CHESTPLATELORE = new ArrayList<>();
+        CHESTPLATELORE.add(ChatColor.DARK_RED + "If you see this...");
+        CHESTPLATELORE.add(ChatColor.DARK_RED + "you are frozen.");
+        chestplateMeta.setLore(CHESTPLATELORE);
+        chestplateMeta.addItemFlags(ItemFlag.HIDE_DYE);
+        chestplateMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        chestplateMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        chestplate.setItemMeta(chestplateMeta);
+        return chestplate;
+    }
+
+    public static ItemStack FrozenIceLEG () {
+        ItemStack leggings = new ItemStack(Material.LEATHER_LEGGINGS);
+        LeatherArmorMeta legMeta = (LeatherArmorMeta) leggings.getItemMeta();
+        legMeta.setDisplayName(ChatColor.AQUA + "" + ChatColor.BOLD + "Ice");
+        legMeta.setColor(Color.fromRGB(255, 85, 0));
+        legMeta.setUnbreakable(true);
+        List<String> LEGGINGSLORE = new ArrayList<>();
+        LEGGINGSLORE.add(ChatColor.DARK_RED + "If you see this...");
+        LEGGINGSLORE.add(ChatColor.DARK_RED + "you are frozen.");
+        legMeta.setLore(LEGGINGSLORE);
+        legMeta.addItemFlags(ItemFlag.HIDE_DYE);
+        legMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        legMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        leggings.setItemMeta(legMeta);
+        return leggings;
+    }
+
+    public static ItemStack FrozenIceBOOTS () {
+        ItemStack boots = new ItemStack(Material.LEATHER_BOOTS);
+        LeatherArmorMeta bootsMeta = (LeatherArmorMeta) boots.getItemMeta();
+        bootsMeta.setDisplayName(ChatColor.AQUA + "" + ChatColor.BOLD + "Ice");
+        bootsMeta.setColor(Color.fromRGB(255, 85, 0));
+        bootsMeta.setUnbreakable(true);
+        List<String> BOOTSELORE = new ArrayList<>();
+        BOOTSELORE.add(ChatColor.DARK_RED + "If you see this...");
+        BOOTSELORE.add(ChatColor.DARK_RED + "you are frozen.");
+        bootsMeta.setLore(BOOTSELORE);
+        bootsMeta.addItemFlags(ItemFlag.HIDE_DYE);
+        bootsMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        bootsMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        boots.setItemMeta(bootsMeta);
+        return boots;
+    }
 
     @EventHandler
     public void onPlayerInteract(BlockPlaceEvent e) {
@@ -45,6 +101,11 @@ public class IceCube implements Listener {
                 if (closestPlayer != null) {
 
                     closestPlayer.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 10, 5));
+
+                    player.getInventory().setChestplate(chestplate);
+                    player.getInventory().setChestplate(leggings);
+                    player.getInventory().setChestplate(boots);
+
                     closestPlayer.addScoreboardTag("frozen");
                     Player finalClosestPlayer = closestPlayer;
                     Bukkit.getScheduler().runTaskLater(lifestealSmp, () -> finalClosestPlayer.removeScoreboardTag("frozen"), 40);
