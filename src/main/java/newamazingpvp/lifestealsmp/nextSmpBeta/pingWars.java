ed773672ef8dd7bf6f4c786d773c6667a593ae2f
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-04-11 19:56:01 -0400

    1

---
+++ src/main/java/newamazingpvp/lifestealsmp/nextSmpBeta/pingWars.java
@@ -0,0 +1,48 @@
+package newamazingpvp.lifestealsmp.nextSmpBeta;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static newamazingpvp.lifestealsmp.LifestealSMP.lifestealSmp;
+
+public class pingWars implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            List<Integer> pings = new ArrayList<>();
+
+            Bukkit.broadcastMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "" + "==========" + "" + ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "" + " [Ping Wars] " + "" + ChatColor.AQUA + "" + ChatColor.BOLD + "" + "==========");
+
+
+            Bukkit.getScheduler().runTaskLater(lifestealSmp, () -> Bukkit.broadcastMessage(ChatColor.GRAY + "" + "Grabbing Info..."), 10);
+
+            for (Player onlinePlayer : player.getServer().getOnlinePlayers()) {
+                pings.add(onlinePlayer.getPing());
+            }
+
+            Bukkit.getScheduler().runTaskLater(lifestealSmp, () -> Bukkit.broadcastMessage(ChatColor.GRAY + "" + "Loading..."), 15);
+
+            
+
+
+
+
+
+            }else{
+                sender.sendMessage("This command can only be executed by a player.");
+            }
+            return true;
+        }
+    }
+
