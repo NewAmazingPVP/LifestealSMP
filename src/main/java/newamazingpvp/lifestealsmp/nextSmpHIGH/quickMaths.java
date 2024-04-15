f90f29e1137b0097dccca63769e173adc6a538e4
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-04-15 19:04:58 -0400

    Update quickMaths.java

--- src/main/java/newamazingpvp/lifestealsmp/nextSmpHIGH/quickMaths.java
+++ src/main/java/newamazingpvp/lifestealsmp/nextSmpHIGH/quickMaths.java
@@ -1,5 +1,6 @@
 package newamazingpvp.lifestealsmp.nextSmpHIGH;
 
+import kotlin.random.URandomKt;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
@@ -8,6 +9,10 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static newamazingpvp.lifestealsmp.LifestealSMP.lifestealSmp;
+
 public class quickMaths implements CommandExecutor {
 
     @Override
@@ -15,23 +20,87 @@ public class quickMaths implements CommandExecutor {
         if (sender instanceof Player) {
             Player player = (Player) sender;
 
-            boolean canAnswer = false;
+            AtomicBoolean canAnswer = new AtomicBoolean(false);
 
             int mathType = 0;
+
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+
+            int ans = 0;
+
+
+            Bukkit.broadcastMessage(ChatColor.GRAY + "Loading...");
+
+            //generates prob type
+            mathType = URandomKt.nextInt(10);
+
+            //generates numbers for problem
+            num1 = URandomKt.nextInt(101);
+            num2 = URandomKt.nextInt(101);
+            num3 = URandomKt.nextInt(101);
 
             for (Player soundLOC1 : Bukkit.getServer().getOnlinePlayers()) {
                 soundLOC1.playSound(soundLOC1.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0F, 0.0F);
             }
 
-            Bukkit.broadcastMessage(ChatColor.GRAY + "Loading...");
-
-            if
-
-
             Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "QUICK MATHS:");
-            Bukkit.broadcastMessage(ChatColor.YELLOW + "QUICK MATHS:");
-            canAnswer = true;
 
+
+            switch (mathType) {
+                case 1:
+                    ans = num1 + num2 + num3;
+                    Bukkit.broadcastMessage(ChatColor.YELLOW + "" + num1 + " + " + num2 + " + " + num3);
+                    break;
+                case 2:
+                    ans = num1 - num2 + num3;
+                    Bukkit.broadcastMessage(ChatColor.YELLOW + "" + num1 + " - " + num2 + " + " + num3);
+                    break;
+                case 3:
+                    ans = num1 + num2 - num3;
+                    Bukkit.broadcastMessage(ChatColor.YELLOW + "" + num1 + " + " + num2 + " - " + num3);
+                    break;
+                case 4:
+                    ans = num1 / num2 + num3;
+                    Bukkit.broadcastMessage(ChatColor.YELLOW + "" + num1 + " / " + num2 + " + " + num3);
+                    break;
+                case 5:
+                    ans = num1 / num2 / num3;
+                    Bukkit.broadcastMessage(ChatColor.YELLOW + "" + num1 + " / " + num2 + " / " + num3);
+                    break;
+                case 6:
+                    ans = num1 * num2 * num3;
+                    Bukkit.broadcastMessage(ChatColor.YELLOW + "" + num1 + " * " + num2 + " * " + num3);
+                    break;
+                case 7:
+                    ans = num1 * num2 / num3;
+                    Bukkit.broadcastMessage(ChatColor.YELLOW + "" + num1 + " * " + num2 + " / " + num3);
+                    break;
+                case 8:
+                    ans = num1 - num2 - num3;
+                    Bukkit.broadcastMessage(ChatColor.YELLOW + "" + num1 + " - " + num2 + " - " + num3);
+                    break;
+                case 9:
+                    ans = num1 + num2 * num3;
+                    Bukkit.broadcastMessage(ChatColor.YELLOW + "" + num1 + " + " + num2 + " * " + num3);
+                    break;
+                default:
+                    Bukkit.broadcastMessage(ChatColor.RED + "error");
+            }
+
+
+
+            canAnswer.set(true);
+
+            Bukkit.getScheduler().runTaskLater(lifestealSmp, () -> canAnswer.set(false), 20*20);
+            Bukkit.getScheduler().runTaskLater(lifestealSmp, () -> Bukkit.broadcastMessage(ChatColor.RED + "Time Up!"), 20*20);
+            int finalAns = ans;
+            Bukkit.getScheduler().runTaskLater(lifestealSmp, () -> Bukkit.broadcastMessage(ChatColor.GREEN + "" + finalAns), 20*20);
+
+            for (Player soundLOC1 : Bukkit.getServer().getOnlinePlayers()) {
+                soundLOC1.playSound(soundLOC1.getLocation(), Sound.ENTITY_VILLAGER_AMBIENT, 1.0F, 2.0F);
+            }
 
 
 
