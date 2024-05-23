749d1c014ab70bc9520134f2ad4988c470c067f0
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-05-22 21:29:36 -0400

    fix?

--- src/main/java/newamazingpvp/lifestealsmp/MinecraftBINGO/BingoCardListener.java
+++ src/main/java/newamazingpvp/lifestealsmp/MinecraftBINGO/BingoCardListener.java
@@ -4,15 +4,36 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BingoCardListener implements Listener {
+
+    public static ItemStack BingoCard(){
+        ItemStack ITEM = new ItemStack(Material.PAINTING);
+        ItemMeta meta = ITEM.getItemMeta();
+        meta.addEnchant(Enchantment.DURABILITY, 1, false);
+        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        meta.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Bingo Card");
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        List<String> LORE = new ArrayList<>();
+        LORE.add(ChatColor.YELLOW + "" + ChatColor.BOLD + "RIGHT CLICK:");
+        LORE.add(ChatColor.LIGHT_PURPLE + "Open Bingo Card");
+        meta.setLore(LORE);
+        ITEM.setItemMeta(meta);
+        return ITEM;
+    }
 
     @EventHandler
     public void playerOpenBINOCard(PlayerInteractEvent e) {
--- src/main/java/newamazingpvp/lifestealsmp/MinecraftBINGO/GiveBingoCardTEMP.java
+++ src/main/java/newamazingpvp/lifestealsmp/MinecraftBINGO/GiveBingoCardTEMP.java
@@ -15,23 +15,11 @@ import org.bukkit.inventory.meta.ItemMeta;
 import java.util.ArrayList;
 import java.util.List;
 
+import static newamazingpvp.lifestealsmp.MinecraftBINGO.BingoCardListener.BingoCard;
+
 public class GiveBingoCardTEMP implements CommandExecutor {
 
 
-    public static ItemStack BingoCard(){
-        ItemStack ITEM = new ItemStack(Material.PAINTING);
-        ItemMeta meta = ITEM.getItemMeta();
-        meta.addEnchant(Enchantment.DURABILITY, 1, false);
-        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-        meta.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Bingo Card");
-        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-        List<String> LORE = new ArrayList<>();
-        LORE.add(ChatColor.YELLOW + "" + ChatColor.BOLD + "RIGHT CLICK:");
-        LORE.add(ChatColor.LIGHT_PURPLE + "Open Bingo Card");
-        meta.setLore(LORE);
-        ITEM.setItemMeta(meta);
-        return ITEM;
-    }
 
 
 
