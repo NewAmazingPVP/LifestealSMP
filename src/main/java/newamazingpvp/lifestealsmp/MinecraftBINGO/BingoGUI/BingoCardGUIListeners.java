b3b07227b357f47aa3c97ed40175288e3b7cb428
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-05-26 17:46:47 -0400

    Update BingoCardGUIListeners.java

--- src/main/java/newamazingpvp/lifestealsmp/MinecraftBINGO/BingoGUI/BingoCardGUIListeners.java
+++ src/main/java/newamazingpvp/lifestealsmp/MinecraftBINGO/BingoGUI/BingoCardGUIListeners.java
@@ -2,13 +2,16 @@ package newamazingpvp.lifestealsmp.MinecraftBINGO.BingoGUI;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import static newamazingpvp.lifestealsmp.MinecraftBINGO.BingoGUI.BingoCardGUI.OpenTheBingoCardGUI;
+import static newamazingpvp.lifestealsmp.MinecraftBINGO.BingoGUI.BingoGUIItems.BingoPickaxeGUI;
 
 public class BingoCardGUIListeners implements Listener {
 
@@ -40,12 +43,20 @@ public class BingoCardGUIListeners implements Listener {
             }
 
             if (itemInHand != null && itemInHand.getType() == Material.PAINTING && itemInHand.hasItemMeta() && itemInHand.getItemMeta().hasDisplayName() && itemInHand.getItemMeta().getDisplayName().equals(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "Open Bingo Card")) {
+                player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1.0f, 2.0f);
                 OpenTheBingoCardGUI(player);
+
+            }
+
+            if (itemInHand != null && itemInHand.getType() == Material.PAINTING && itemInHand.hasItemMeta() && itemInHand.getItemMeta().hasDisplayName() && itemInHand.getItemMeta().getDisplayName().equals(ChatColor.RED + "" + ChatColor.BOLD + "Bingo Pickaxe")) {
+                player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1.0f, 2.0f);
+                player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "+1 ");
+                player.getInventory().addItem(BingoPickaxeGUI());
             }
 
 
-        }
 
+        }
 
 
 
