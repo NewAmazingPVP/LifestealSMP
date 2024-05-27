10b0d1b8c678896e9cfa57134cc7f14d3017fd93
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-05-27 18:43:06 -0400

    Update BingoRecipes.java

--- src/main/java/newamazingpvp/lifestealsmp/MinecraftBINGO/BingoRecipes.java
+++ src/main/java/newamazingpvp/lifestealsmp/MinecraftBINGO/BingoRecipes.java
@@ -14,7 +14,7 @@ public class BingoRecipes {
     public static void registerBingoRecipes(){
 
         ItemStack bingoPic = new ItemStack(BingoPickaxe());
-        ShapedRecipe bingoPicRecipe = new ShapedRecipe(new NamespacedKey(lifestealSmp, "extra_heart"), bingoPic);
+        ShapedRecipe bingoPicRecipe = new ShapedRecipe(new NamespacedKey(lifestealSmp, "bingo_heart"), bingoPic);
         bingoPicRecipe.shape("CCC", "ASA", "ASA");
         bingoPicRecipe.setIngredient('S', Material.STICK);
         bingoPicRecipe.setIngredient('A', Material.AIR);
