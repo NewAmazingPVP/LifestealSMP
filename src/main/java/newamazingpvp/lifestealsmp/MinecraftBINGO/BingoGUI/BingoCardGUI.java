9ce68848514b3d7dfb3c33c64fc5df00dec658dc
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-05-27 11:15:54 -0400

    fix

--- src/main/java/newamazingpvp/lifestealsmp/MinecraftBINGO/BingoGUI/BingoCardGUI.java
+++ src/main/java/newamazingpvp/lifestealsmp/MinecraftBINGO/BingoGUI/BingoCardGUI.java
@@ -23,51 +23,51 @@ public class BingoCardGUI {
 
         bingoCardGUI.setItem(0,orangeGlassGUI());
         bingoCardGUI.setItem(1,orangeGlassGUI());
-        bingoCardGUI.setItem(2,orangeGlassGUI());
-        bingoCardGUI.setItem(3,orangeGlassGUI());
-        bingoCardGUI.setItem(4,orangeGlassGUI());
-        bingoCardGUI.setItem(5,orangeGlassGUI());
-        bingoCardGUI.setItem(6,orangeGlassGUI());
+        bingoCardGUI.setItem(2,noChallengeGenerated());
+        bingoCardGUI.setItem(3,noChallengeGenerated());
+        bingoCardGUI.setItem(4,noChallengeGenerated());
+        bingoCardGUI.setItem(5,noChallengeGenerated());
+        bingoCardGUI.setItem(6,noChallengeGenerated());
         bingoCardGUI.setItem(7,orangeGlassGUI());
         bingoCardGUI.setItem(8,orangeGlassGUI());
 
         bingoCardGUI.setItem(9,orangeGlassGUI());
-        bingoCardGUI.setItem(10,noChallengeGenerated());
+        bingoCardGUI.setItem(10,orangeGlassGUI());
         bingoCardGUI.setItem(11,noChallengeGenerated());
         bingoCardGUI.setItem(12,noChallengeGenerated());
         bingoCardGUI.setItem(13,noChallengeGenerated());
-        bingoCardGUI.setItem(14,orangeGlassGUI());
-        bingoCardGUI.setItem(15,orangeGlassGUI());
+        bingoCardGUI.setItem(14,noChallengeGenerated());
+        bingoCardGUI.setItem(15,noChallengeGenerated());
         bingoCardGUI.setItem(16,orangeGlassGUI());
         bingoCardGUI.setItem(17,orangeGlassGUI());
 
         bingoCardGUI.setItem(18,orangeGlassGUI());
-        bingoCardGUI.setItem(19,noChallengeGenerated());
+        bingoCardGUI.setItem(19,orangeGlassGUI());
         bingoCardGUI.setItem(20,noChallengeGenerated());
         bingoCardGUI.setItem(21,noChallengeGenerated());
         bingoCardGUI.setItem(22,noChallengeGenerated());
-        bingoCardGUI.setItem(23,orangeGlassGUI());
-        bingoCardGUI.setItem(24,orangeGlassGUI());
+        bingoCardGUI.setItem(23,noChallengeGenerated());
+        bingoCardGUI.setItem(24,noChallengeGenerated());
         bingoCardGUI.setItem(25,orangeGlassGUI());
         bingoCardGUI.setItem(26,orangeGlassGUI());
 
         bingoCardGUI.setItem(27,orangeGlassGUI());
-        bingoCardGUI.setItem(28,noChallengeGenerated());
+        bingoCardGUI.setItem(28,orangeGlassGUI());
         bingoCardGUI.setItem(29,noChallengeGenerated());
         bingoCardGUI.setItem(30,noChallengeGenerated());
         bingoCardGUI.setItem(31,noChallengeGenerated());
-        bingoCardGUI.setItem(32,orangeGlassGUI());
-        bingoCardGUI.setItem(33,orangeGlassGUI());
+        bingoCardGUI.setItem(32,noChallengeGenerated());
+        bingoCardGUI.setItem(33,noChallengeGenerated());
         bingoCardGUI.setItem(34,orangeGlassGUI());
         bingoCardGUI.setItem(35,orangeGlassGUI());
 
         bingoCardGUI.setItem(36,orangeGlassGUI());
-        bingoCardGUI.setItem(37,noChallengeGenerated());
+        bingoCardGUI.setItem(37,orangeGlassGUI());
         bingoCardGUI.setItem(38,noChallengeGenerated());
         bingoCardGUI.setItem(39,noChallengeGenerated());
         bingoCardGUI.setItem(40,noChallengeGenerated());
-        bingoCardGUI.setItem(41,orangeGlassGUI());
-        bingoCardGUI.setItem(42,orangeGlassGUI());
+        bingoCardGUI.setItem(41,noChallengeGenerated());
+        bingoCardGUI.setItem(42,noChallengeGenerated());
         bingoCardGUI.setItem(43,orangeGlassGUI());
         bingoCardGUI.setItem(44,orangeGlassGUI());
 
