2b0e508d8ad4d34f0bcf16810c2b3c27ac66bd94
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-05-28 17:32:30 -0400

    Update BingoMessageEvents.java

--- src/main/java/newamazingpvp/lifestealsmp/MinecraftBINGO/BingoMessageEvents.java
+++ src/main/java/newamazingpvp/lifestealsmp/MinecraftBINGO/BingoMessageEvents.java
@@ -13,8 +13,8 @@ public class BingoMessageEvents {
     public static void bingoChallengeDone(Player player,String challengeName){
 
         player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + challengeName + " Complete!");
-        player.playSound(player.getLocation(), Sound.ENTITY_FIREWORK_ROCKET_BLAST, 3.0f, 1.0f);
-        Bukkit.getScheduler().runTaskLater(lifestealSmp, () -> player.playSound(player.getLocation(), Sound.ENTITY_FIREWORK_ROCKET_BLAST, 3.0f, 1.0f), 20);
+        player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BIT, 3.0f, 1.0f);
+        Bukkit.getScheduler().runTaskLater(lifestealSmp, () -> player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BIT, 3.0f, 2.0f), 10);
 
 
     }
