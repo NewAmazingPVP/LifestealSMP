e2c16350edd598435732412cbae9dfdc9e1066f9
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-05-29 21:21:45 -0400

    1

--- src/main/java/newamazingpvp/lifestealsmp/MinecraftBINGO/BingoRecipes.java
+++ src/main/java/newamazingpvp/lifestealsmp/MinecraftBINGO/BingoRecipes.java
@@ -7,6 +7,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 
 import static newamazingpvp.lifestealsmp.LifestealSMP.lifestealSmp;
+import static newamazingpvp.lifestealsmp.MinecraftBINGO.customBingoItems.BingoCard;
 import static newamazingpvp.lifestealsmp.MinecraftBINGO.customBingoItems.BingoPickaxe;
 
 public class BingoRecipes {
@@ -21,6 +22,14 @@ public class BingoRecipes {
         bingoPicRecipe.setIngredient('C', Material.COPPER_BLOCK);
         Bukkit.addRecipe(bingoPicRecipe);
 
+
+        ItemStack bingoCard = new ItemStack(BingoCard());
+        ShapedRecipe bingoCardR = new ShapedRecipe(new NamespacedKey(lifestealSmp, "bingo_card"), bingoPic);
+        bingoCardR.shape("OOO", "OOO", "OOO");
+        bingoCardR.setIngredient('O', Material.OAK_PLANKS);
+        Bukkit.addRecipe(bingoCardR);
+
+        
 
     }
 
