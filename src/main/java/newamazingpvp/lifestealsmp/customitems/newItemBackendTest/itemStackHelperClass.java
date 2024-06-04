4f5e24f95951a5636f23ab364b613e4982ae7bd3
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-06-04 17:00:21 -0400

    lol

---
+++ src/main/java/newamazingpvp/lifestealsmp/customitems/newItemBackendTest/itemStackHelperClass.java
@@ -0,0 +1,55 @@
+package newamazingpvp.lifestealsmp.customitems.newItemBackendTest;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class itemStackHelperClass {
+
+    //idk what i was doing this will def not work 100%
+
+    public static ItemStack makeNewItemStack(Material itemType, String itemName, String abilityTriggerType, String abilityDescriptionLine1 , String abilityDescriptionLine2 , Boolean Glowing, Boolean unbreakable, Boolean hideAttributes){
+
+        ItemStack ITEM = new ItemStack(itemType);
+        ItemMeta META = ITEM.getItemMeta();
+
+        META.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + itemName);
+
+        List<String> LORE = new ArrayList<>();
+        LORE.add(ChatColor.GOLD + "" + ChatColor.BOLD + abilityTriggerType + ":");
+        LORE.add(ChatColor.DARK_PURPLE + abilityDescriptionLine1);
+        LORE.add(ChatColor.DARK_PURPLE + abilityDescriptionLine2);
+        META.setLore(LORE);
+
+        if(Glowing){
+            META.addEnchant(Enchantment.DURABILITY, 1, false);
+            META.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        }
+
+        if(unbreakable){
+            META.setUnbreakable(true);
+            META.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        }
+
+        if(hideAttributes){
+            META.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        }
+
+        ITEM.setItemMeta(META);
+
+
+        return ITEM;
+    }
+
+
+
+
+}
+
+
