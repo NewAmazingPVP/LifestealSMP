9dde1c0f14e7c2ba78bca6bdcd06f245a2d6ce82
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-06-06 20:55:55 -0400

    1

--- src/main/java/newamazingpvp/lifestealsmp/customitems/LifestealStick.java
+++ src/main/java/newamazingpvp/lifestealsmp/customitems/LifestealStick.java
@@ -1,6 +1,7 @@
 package newamazingpvp.lifestealsmp.customitems;
 
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,17 +14,25 @@ public class LifestealStick implements Listener {
     @EventHandler
     public void playerHitPlayer(EntityDamageByEntityEvent e) {
 
+        Entity damagedPlayer = e.getEntity();
+
         if (e.getDamager() instanceof Player) {
             Player player = (Player) e.getDamager();
             ItemStack itemInHand = player.getItemInHand();
             ItemMeta meta = itemInHand.getItemMeta();
             if (meta != null && meta.getLore() != null && meta.getLore().toString().contains("You will heal " + ChatColor.RED + "1❤")) {
+                if(damagedPlayer instanceof Player){
 
-                player.setHealth(player.getHealth() + 1);
+                    player.setHealth(player.getHealth() + 1);
+
+                }
+
+
 
 
             }
         }
     }
 }
+
 
