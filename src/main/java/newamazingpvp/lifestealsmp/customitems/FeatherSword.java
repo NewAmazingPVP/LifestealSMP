4eba8392ef71d4d13f9f8a3b972d9f5b43505c6d
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-06-08 14:37:20 -0400

    Update FeatherSword.java

--- src/main/java/newamazingpvp/lifestealsmp/customitems/FeatherSword.java
+++ src/main/java/newamazingpvp/lifestealsmp/customitems/FeatherSword.java
@@ -63,7 +63,7 @@ public class FeatherSword implements Listener {
 
                     player.getLocation().getWorld().spawnParticle(Particle.EXPLOSION_NORMAL, player.getLocation(), 10);
                     player.playSound(player.getLocation(), Sound.ENTITY_BLAZE_SHOOT, 5.0f, 2.0f);
-                    player.setVelocity(player.getLocation().getDirection().multiply(2));
+                    player.setVelocity(player.getLocation().getDirection().multiply(5));
                     setTeleportCooldown(player);
 
                     //if (isSafeLocation(targetLocation)) {
