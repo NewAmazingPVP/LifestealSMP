50351eceb316d5f66d53ac660aff455d6d2af275
NewAmazingPVP <gamingofpat@gmail.com>
2024-06-13 22:29:19 -0400

    Update QuarryArmor.java

--- src/main/java/newamazingpvp/lifestealsmp/customitems/armorlisteners/QuarryArmor.java
+++ src/main/java/newamazingpvp/lifestealsmp/customitems/armorlisteners/QuarryArmor.java
@@ -12,16 +12,8 @@ import org.bukkit.potion.PotionEffectType;
 
 public class QuarryArmor implements Listener {
 
-    public static boolean quarryIsHelmetOn = false;
-    public static boolean quarryIsChestplateOn = false;
-    public static boolean quarryIsLeggingsOn = false;
-    public static boolean quarryIsBootsOn = false;
-
-    private static final boolean doubleOreDrops = false;
-
     @EventHandler
     public void onArmorChange(PlayerMoveEvent event) {
-
         Player player = event.getPlayer();
 
         ItemStack helmet = player.getInventory().getHelmet();
@@ -29,81 +21,23 @@ public class QuarryArmor implements Listener {
         ItemStack leggings = player.getInventory().getLeggings();
         ItemStack boots = player.getInventory().getBoots();
 
-        if(chestplate.getType() == Material.AIR){
-            quarryIsChestplateOn = false;
-        }
-
-        if(helmet.getType() == Material.AIR){
-            quarryIsHelmetOn = false;
-        }
-
-        if(leggings.getType() == Material.AIR){
-            quarryIsLeggingsOn = false;
-        }
-
-        if(boots.getType() == Material.AIR){
-            quarryIsBootsOn = false;
-        }
-
-        ItemMeta helmetMeta = helmet.getItemMeta();
-        ItemMeta chestplateMeta = chestplate.getItemMeta();
-        ItemMeta leggingMetas = leggings.getItemMeta();
-        ItemMeta bootsMeta = boots.getItemMeta();
-
-
-        quarryIsHelmetOn = helmet.getType() == Material.LEATHER_HELMET && helmetMeta != null && helmetMeta.getLore() != null && helmetMeta.getLore().toString().contains("You also have unlimited haste 3.");
-
-
-        quarryIsChestplateOn = chestplate.getType() == Material.LEATHER_CHESTPLATE && chestplateMeta != null && chestplateMeta.getLore() != null && chestplateMeta.getLore().toString().contains("You also have unlimited haste 3.");
-
-
-        quarryIsLeggingsOn = leggings.getType() == Material.LEATHER_LEGGINGS && leggingMetas != null && leggingMetas.getLore() != null && leggingMetas.getLore().toString().contains("You also have unlimited haste 3.");
-
-
-        quarryIsBootsOn = boots.getType() == Material.LEATHER_BOOTS && bootsMeta != null && bootsMeta.getLore() != null && bootsMeta.getLore().toString().contains("You also have unlimited haste 3.");
-
-
+        boolean quarryIsHelmetOn = isQuarryArmorPieceOn(helmet, Material.LEATHER_HELMET);
+        boolean quarryIsChestplateOn = isQuarryArmorPieceOn(chestplate, Material.LEATHER_CHESTPLATE);
+        boolean quarryIsLeggingsOn = isQuarryArmorPieceOn(leggings, Material.LEATHER_LEGGINGS);
+        boolean quarryIsBootsOn = isQuarryArmorPieceOn(boots, Material.LEATHER_BOOTS);
 
         if (quarryIsHelmetOn && quarryIsChestplateOn && quarryIsLeggingsOn && quarryIsBootsOn) {
             player.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 100000, 2));
         } else {
             player.removePotionEffect(PotionEffectType.FAST_DIGGING);
         }
-
-
     }
 
-
+    private boolean isQuarryArmorPieceOn(ItemStack item, Material requiredMaterial) {
+        if (item == null || item.getType() != requiredMaterial) {
+            return false;
+        }
+        ItemMeta meta = item.getItemMeta();
+        return meta != null && meta.getLore() != null && meta.getLore().toString().contains("You also have unlimited haste 3.");
+    }
 }
-
-
-
-
-
-
-
-
-
-        /*private static boolean isHelmetOn(){
-
-        }
-
-        private static boolean isChestplateOn(){
-
-        }
-
-        private static boolean isLeggingsOn(){
-
-        }
-
-        private static boolean osBootsOn(){
-
-        }
-
-
-
-
-
-    }
-
-}*/
