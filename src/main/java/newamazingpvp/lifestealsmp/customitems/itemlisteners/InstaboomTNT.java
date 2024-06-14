066962c994948b6f31b2769023352f2ed3d6aca1
NewAmazingPVP <gamingofpat@gmail.com>
2024-06-13 22:03:43 -0400

    Update InstaboomTNT.java

--- src/main/java/newamazingpvp/lifestealsmp/customitems/itemlisteners/InstaboomTNT.java
+++ src/main/java/newamazingpvp/lifestealsmp/customitems/itemlisteners/InstaboomTNT.java
@@ -1,6 +1,5 @@
 package newamazingpvp.lifestealsmp.customitems.itemlisteners;
 
-import newamazingpvp.lifestealsmp.utility.CooldownManager;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,79 +10,82 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static newamazingpvp.lifestealsmp.customitems.itemlisteners.FeatherSword.getString;
+
 public class InstaboomTNT implements Listener {
 
-    private final Map<Player, CooldownManager> tntCooldowns = new HashMap<>();
-    private final double tntCooldownTime = 10.0;
+
+    private final Map<Player, Long> tntCooldowns = new HashMap<>();
+    private final double tntCooldownTime = 10000;
 
     @EventHandler
-    public void onBlockPlace(BlockPlaceEvent event) {
-        Player player = event.getPlayer();
-        ItemStack itemInMainHand = player.getInventory().getItemInMainHand();
-        ItemMeta meta = itemInMainHand.getItemMeta();
-        Location location = event.getBlock().getLocation();
+    public void playerPlaceBlock(BlockPlaceEvent e) {
 
-        if (isInstaBoomTNT(meta)) {
-            event.setCancelled(true);
+        Player player = e.getPlayer();
+        ItemStack item = player.getInventory().getItemInMainHand();
+        ItemMeta meta = item.getItemMeta();
+        Location location = e.getBlock().getLocation();
 
-            CooldownManager cooldown = tntCooldowns.get(player);
-            if (cooldown == null) {
-                cooldown = new CooldownManager(tntCooldownTime);
-                tntCooldowns.put(player, cooldown);
-            }
 
-            if (!cooldown.isOnCooldown()) {
-                triggerInstaBoom(player, location);
-                cooldown.setCooldown(tntCooldownTime);
-                tntCooldowns.put(player, cooldown);
-                updateItemStack(player, itemInMainHand);
-            } else {
-                player.sendMessage(ChatColor.RED + "You must wait " + cooldown.getRemainingSeconds() + " seconds before using InstaBoom TNT again.");
-            }
-        } else if (isInstaBoomTNT(player.getInventory().getItemInOffHand().getItemMeta())) {
-            event.setCancelled(true);
-        }
-    }
 
-    private boolean isInstaBoomTNT(ItemMeta meta) {
-        return meta != null && meta.hasDisplayName() && meta.toString().contains("InstaBoom TNT");
-    }
+        if (meta != null && meta.hasDisplayName() && meta.getDisplayName().equals(ChatColor.GOLD + "" + ChatColor.BOLD + "InstaBoom TNT")) {
+            e.setCancelled(true);
+            if (isTeleportCooldownExpired(player)) {
+                setTeleportCooldown(player);
+                location.getWorld().spawnParticle(Particle.EXPLOSION_HUGE, location, 10);
+                for (Player onlineplayer : Bukkit.getOnlinePlayers()) {
+                    onlineplayer.playSound(location, Sound.ENTITY_GENERIC_EXPLODE, 5.0f, 1.0f);
+                    onlineplayer.playSound(location, Sound.ENTITY_ELDER_GUARDIAN_CURSE, 5.0f, 2.0f);
+                    List<Player> nearbyPlayers = (List<Player>) location.getWorld().getNearbyPlayers(location, 3);
+                    for (Player playernear : nearbyPlayers) {
+                        playernear.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS, 20, 1));
+                        Vector direction = playernear.getLocation().toVector().subtract(location.toVector()).normalize();
+                        Vector velocity = direction.multiply(20);
+                        double maxHeight = 1.0;
+                        if (velocity.getY() > maxHeight) {
+                            velocity.setY(maxHeight);
+                        }
+                        playernear.setVelocity(velocity);
 
-    private void triggerInstaBoom(Player player, Location location) {
-        World world = location.getWorld();
-        if (world != null) {
-            world.spawnParticle(Particle.EXPLOSION_HUGE, location, 10);
-            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-                onlinePlayer.playSound(location, Sound.ENTITY_GENERIC_EXPLODE, 5.0f, 1.0f);
-                onlinePlayer.playSound(location, Sound.ENTITY_ELDER_GUARDIAN_CURSE, 5.0f, 2.0f);
-                List<Player> nearbyPlayers = (List<Player>) world.getNearbyPlayers(location, 3);
-                for (Player nearbyPlayer : nearbyPlayers) {
-                    applyEffectsAndKnockback(nearbyPlayer, location);
+                    }
                 }
+                if (item.getAmount() > 1) {
+                    item.setAmount(item.getAmount() - 1);
+                    player.getInventory().setItemInMainHand(item);
+                } else {
+                    player.getInventory().setItemInMainHand(null);
+                }
+            }else{
+                player.sendMessage(ChatColor.RED + "You must wait " + cooldownRemainingTime(player) + " for the cooldown to finish before using instaboom tnt again.");
             }
+
         }
     }
 
-    private void applyEffectsAndKnockback(Player player, Location explosionLocation) {
-        player.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS, 20, 1));
-        Vector direction = player.getLocation().toVector().subtract(explosionLocation.toVector()).normalize();
-        Vector velocity = direction.multiply(20);
-        if (velocity.getY() > 1.0) {
-            velocity.setY(1.0);
+    private boolean isTeleportCooldownExpired(Player player) {
+        if (tntCooldowns.containsKey(player)) {
+            long lastTeleportTime = tntCooldowns.get(player);
+            long currentTime = System.currentTimeMillis();
+            return currentTime - lastTeleportTime >= tntCooldownTime;
         }
-        player.setVelocity(velocity);
+        return true;
     }
 
-    private void updateItemStack(Player player, ItemStack item) {
-        if (item.getAmount() > 1) {
-            item.setAmount(item.getAmount() - 1);
-        } else {
-            player.getInventory().setItemInMainHand(null);
-        }
+
+    private void setTeleportCooldown(Player player) {
+        tntCooldowns.put(player, System.currentTimeMillis());
     }
+
+    private String cooldownRemainingTime(Player player) {
+        return getString(player, tntCooldowns, (long) tntCooldownTime);
+    }
+
+
+
 }
