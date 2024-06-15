54e5d84bced8e846ce1efc131b64beaa07642d4b
NewAmazingPVP <gamingofpat@gmail.com>
2024-06-15 18:09:28 -0400

    trade beta implementation
    trade beta implementation

---
+++ src/main/java/newamazingpvp/lifestealsmp/command/Trade.java
@@ -0,0 +1,33 @@
+package newamazingpvp.lifestealsmp.command;
+
+import newamazingpvp.lifestealsmp.utility.TradeManager;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Trade implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("trade")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (args.length != 1) {
+                    player.sendMessage("Usage: /trade <player>");
+                    return true;
+                }
+
+                Player target = Bukkit.getPlayer(args[0]);
+                if (target == null || !target.isOnline()) {
+                    player.sendMessage("Player not found or not online.");
+                    return true;
+                }
+
+                TradeManager.initiateTrade(player, target);
+                return true;
+            }
+        }
+        return false;
+    }
+}
