e81eeca8d99cf3149e3fc3729e9a5035cc57fcd3
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-06-16 14:38:46 -0400

    Create clientSideBlock.java

---
+++ src/main/java/newamazingpvp/lifestealsmp/testing/clientSideBlock.java
@@ -0,0 +1,14 @@
+package newamazingpvp.lifestealsmp.testing;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.Plugin;
+
+public class clientSideBlock implements Listener {
+    
+}
