414f00fcc6ef1cbf3099b101034e5a618b40a370
NewAmazingPVP <gamingofpat@gmail.com>
2024-06-16 22:45:29 -0400

    Update staffBeamTexture.java

--- src/main/java/newamazingpvp/lifestealsmp/customitems/MagicStaffs/MagicStaffUtils/staffBeamTexture.java
+++ src/main/java/newamazingpvp/lifestealsmp/customitems/MagicStaffs/MagicStaffUtils/staffBeamTexture.java
@@ -10,7 +10,7 @@ import org.bukkit.util.Vector;
 public class staffBeamTexture {
     public static void beamTextureMaker(Player player, Location location, Vector attackerLookDir,Color color1, Float size1, Color color2, Float Size2) {
 
-        for (double i = 0; i < 30; i+=.5) {
+        for (double i = 0; i < 150; i++) {
             location.add(attackerLookDir);
             for (Player player2 : Bukkit.getOnlinePlayers()) {
                 player2.getWorld().spawnParticle(Particle.REDSTONE, location, 0, new Particle.DustOptions(color1, size1));
