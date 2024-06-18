0fcc79f0309b3f2eae5a9cfe80772a5fcfce3229
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-06-18 15:21:11 -0400

    1

---
+++ src/main/java/newamazingpvp/lifestealsmp/CustomMobs/SpawnCustomMobCommand.java
@@ -0,0 +1,41 @@
+package newamazingpvp.lifestealsmp.CustomMobs;
+
+import newamazingpvp.lifestealsmp.CustomMobs.Mobs.lightningZombieCreate;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import static newamazingpvp.lifestealsmp.customitems.utils.ItemStacks.*;
+import static newamazingpvp.lifestealsmp.runes.RunesDrops.*;
+import static newamazingpvp.lifestealsmp.runes.RunesDrops.airRune;
+
+public class SpawnCustomMobCommand implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        Location location = null;
+
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            location = player.getLocation();
+        } else if (sender instanceof BlockCommandSender) {
+            BlockCommandSender blockCommandSender = (BlockCommandSender) sender;
+            location = blockCommandSender.getBlock().getLocation();
+        }
+
+        new lightningZombieCreate(location);
+
+        return true;
+
+
+    }
+}
+
