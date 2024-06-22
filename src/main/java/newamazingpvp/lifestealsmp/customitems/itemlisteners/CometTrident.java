5615214d4cd1656b39b2091cd37ca7a8c060382c
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-06-22 14:41:36 -0400

    1

---
+++ src/main/java/newamazingpvp/lifestealsmp/customitems/itemlisteners/CometTrident.java
@@ -0,0 +1,41 @@
+package newamazingpvp.lifestealsmp.customitems.itemlisteners;
+
+import newamazingpvp.lifestealsmp.utility.CooldownManager;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CometTrident implements Listener {
+
+    private final Map<Player, Entity> linkPlayerWithComet = new HashMap<>();
+    private final Map<Player, CooldownManager> wandCooldowns = new HashMap<>();
+
+
+    @EventHandler
+    public void onPlayerRightClick(PlayerInteractEvent e) {
+        Player player = e.getPlayer();
+        ItemMeta meta = (ItemMeta) e.getItem();
+
+        if ((e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) && e.hasItem() && e.hasItem() && e.getItem().getType() == Material.TRIDENT) {
+            if (meta.getLore().toString().contains("Summons a comet that will fly into")){
+
+
+                float pitch = player.getLocation().getPitch();
+                player.sendMessage("The player is looking up/down by " + pitch + " degrees.");
+
+
+
+
+
+            }
+        }
+    }
+}
