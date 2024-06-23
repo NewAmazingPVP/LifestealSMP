babaa871f89b3e11bcad6720ed88fe82dc0e0e60
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-06-22 21:17:55 -0400

    Update CometTrident.java

--- src/main/java/newamazingpvp/lifestealsmp/customitems/itemlisteners/CometTrident.java
+++ src/main/java/newamazingpvp/lifestealsmp/customitems/itemlisteners/CometTrident.java
@@ -1,6 +1,7 @@
 package newamazingpvp.lifestealsmp.customitems.itemlisteners;
 
 import newamazingpvp.lifestealsmp.utility.CooldownManager;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -10,6 +11,7 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.util.Vector;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,6 +27,9 @@ public class CometTrident implements Listener {
         Player player = e.getPlayer();
         ItemStack itemInMainHand = player.getInventory().getItemInMainHand();
         ItemMeta meta = itemInMainHand.getItemMeta();
+        float pitch = player.getLocation().getPitch();
+        Location spawnLoc = null;
+        Vector direction = player.getEyeLocation().getDirection();
 
         if ((e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) && e.hasItem() && e.hasItem() && e.getItem().getType() == Material.TRIDENT) {
             if (meta.getLore().toString().contains("Summons a comet that will fly into")){
@@ -32,13 +37,17 @@ public class CometTrident implements Listener {
 
                 e.setCancelled(true);
 
-                float pitch = player.getLocation().getPitch();
 
                 if(pitch < 0 ){
-                    pitch = Math.abs(pitch + 90);
+                    pitch = Math.abs(pitch) + 90;
                 }
 
+                spawnLoc = player.getLocation();
+                spawnLoc.add(0, 10, 0);
+
+
                 player.sendMessage("The player is looking up/down by " + pitch + " degrees.");
+                player.sendMessage("Direction player is looking is" + direction);
 
 
 
