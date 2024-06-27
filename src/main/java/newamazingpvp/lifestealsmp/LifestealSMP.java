2749caffca55203861a67a3aa23666dfaeaf9c9f
NewAmazingPVP <gamingofpat@gmail.com>
2024-06-27 15:34:49 -0400

    server opening
    server opening

--- src/main/java/newamazingpvp/lifestealsmp/LifestealSMP.java
+++ src/main/java/newamazingpvp/lifestealsmp/LifestealSMP.java
@@ -154,6 +154,7 @@ public final class LifestealSMP extends JavaPlugin implements Listener, PluginMe
         getServer().getPluginManager().registerEvents(new DisableNetherite(), this);
         getServer().getPluginManager().registerEvents(new NewbieProgression(), this);
         getServer().getPluginManager().registerEvents(new AntiEnd(), this);
+        getServer().getPluginManager().registerEvents(new ServerOpening(), this);
         startTPSTracking();
         getServer().getScheduler().runTaskTimer(this, Utils::adjustPerformance, 120, 1);
         getCommand("trade").setExecutor(new Trade());
---
+++ src/main/java/newamazingpvp/lifestealsmp/listener/ServerOpening.java
@@ -0,0 +1,34 @@
+package newamazingpvp.lifestealsmp.listener;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import static newamazingpvp.lifestealsmp.utility.TimeManager.SEASON_START_TIME;
+import static newamazingpvp.lifestealsmp.utility.TimeManager.formatDuration;
+
+public class ServerOpening implements Listener {
+    @EventHandler
+    public void onJoin(PlayerLoginEvent event) {
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("America/New_York"));
+        if (!Bukkit.getWhitelistedPlayers().contains(event.getPlayer())) {
+            return;
+        }
+        if (now.isBefore(SEASON_START_TIME)) {
+            Duration duration = Duration.between(now, SEASON_START_TIME);
+            String timeRemaining = formatDuration(duration);
+            event.disallow(PlayerLoginEvent.Result.KICK_OTHER, ChatColor.GREEN + "New Lifesteal season is starting in " + timeRemaining + ChatColor.AQUA + " Join discord https://discord.gg/PN8egFY3ap for more info!");
+        }
+    }
+}
