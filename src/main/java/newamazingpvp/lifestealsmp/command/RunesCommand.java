ce8c788fcba0728aa376790e3d1007f78f7fa699
NewAmazingPVP <gamingofpat@gmail.com>
2024-06-27 16:01:58 -0400

    /runes
    /runes

---
+++ src/main/java/newamazingpvp/lifestealsmp/command/RunesCommand.java
@@ -0,0 +1,19 @@
+package newamazingpvp.lifestealsmp.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import static newamazingpvp.lifestealsmp.runes.RuneHandler.inv;
+
+public class RunesCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(sender instanceof Player){
+            Player p = (Player) sender;
+            p.openInventory(inv);
+        }
+        return true;
+    }
+}
