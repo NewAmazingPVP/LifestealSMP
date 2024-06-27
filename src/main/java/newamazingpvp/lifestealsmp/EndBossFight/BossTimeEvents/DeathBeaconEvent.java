9b570a490a5b15c1221d771c514c8f007a7dd042
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-06-27 18:13:43 -0400

    Update DeathBeaconEvent.java

--- src/main/java/newamazingpvp/lifestealsmp/EndBossFight/BossTimeEvents/DeathBeaconEvent.java
+++ src/main/java/newamazingpvp/lifestealsmp/EndBossFight/BossTimeEvents/DeathBeaconEvent.java
@@ -1,17 +1,19 @@
 package newamazingpvp.lifestealsmp.EndBossFight.BossTimeEvents;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Sound;
+import org.bukkit.*;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import static newamazingpvp.lifestealsmp.EndBossFight.EndBossFightVariables.*;
 import static newamazingpvp.lifestealsmp.LifestealSMP.lifestealSmp;
 
 public class DeathBeaconEvent implements Listener {
@@ -26,7 +28,60 @@ public class DeathBeaconEvent implements Listener {
     private static Random rand = new Random();
 
 
-    //TIMER CONTROLLER
+
+
+    @EventHandler
+    public void playerPunchDeathBeacon(PlayerInteractEvent e) {
+
+        Player player = e.getPlayer();
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.LEFT_CLICK_BLOCK) {
+            Location clickedBlockLoc = e.getClickedBlock().getLocation();
+
+            playerBreakDeathBeacon(player, clickedBlockLoc);
+
+
+        }
+    }
+
+
+
+
+    private void playerBreakDeathBeacon(Player player, Location clickedBlockLoc){
+
+        if(clickedBlockLoc == deathBeaconLocRED || clickedBlockLoc == deathBeaconLocYellow || clickedBlockLoc == deathBeaconLocGreen || clickedBlockLoc == deathBeaconLocBlue){
+
+            Block block = (Block) clickedBlockLoc;
+            block.setType(Material.AIR);
+            clickedBlockLoc.getWorld().spawnParticle(Particle.CLOUD, player.getLocation(), 10); 
+            String name = player.getName();
+            player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "You Broke The Death Beacon!");
+            player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 2.0f, 2.0f);
+            Bukkit.getOnlinePlayers().forEach(p -> p.sendTitle(ChatColor.GREEN + "" + ChatColor.BOLD + "" + name + " Broke The Beacon!", " " , 10, 40, 5));
+            Bukkit.broadcastMessage(ChatColor.DARK_GREEN + "" + ChatColor.BOLD + name + " Has Broken The Death Beacon!");
+
+            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                onlinePlayer.playSound(onlinePlayer.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 2.0f, 2.0f);
+
+            }
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static void runDeathBeaconTimer(Player player) {
 
         if (!isRunning) {
@@ -35,6 +90,28 @@ public class DeathBeaconEvent implements Listener {
 
             int randomSideIndex = rand.nextInt(arenaSides.size());
             String selectedSide = arenaSides.get(randomSideIndex);
+
+
+
+
+
+            if(selectedSide == "Red"){
+                Block block = (Block) deathBeaconLocRED;
+                block.setType(Material.BEACON);
+
+            }else if(selectedSide == "Yellow"){
+                Block block = (Block) deathBeaconLocYellow;
+                block.setType(Material.BEACON);
+
+            }else if(selectedSide == "Green"){
+                Block block = (Block) deathBeaconLocGreen;
+                block.setType(Material.BEACON);
+
+            }else if(selectedSide == "Blue"){
+                Block block = (Block) deathBeaconLocBlue;
+                block.setType(Material.BEACON);
+
+            }
 
 
 
