67ba83eab5ca628ae337a0412bcb6987da5321db
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-06-28 22:52:25 -0400

    admin runes
    admin runes

---
+++ src/main/java/newamazingpvp/lifestealsmp/command/AdminRunes.java
@@ -0,0 +1,28 @@
+package newamazingpvp.lifestealsmp.command;
+
+import newamazingpvp.lifestealsmp.runes.Rune;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+import static newamazingpvp.lifestealsmp.runes.RuneHandler.*;
+
+public class AdminRunes implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(sender instanceof Player){
+            Player p = (Player) sender;
+            Inventory inv = Bukkit.createInventory(null, 27, ChatColor.GOLD + "Admin");
+            for(Rune r: runes){
+                inv.addItem(createRuneItem(r));
+            }
+            p.openInventory(inv);
+            return true;
+        }
+        return false;
+    }
+}
