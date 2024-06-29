4f55456e37b1eec216cc24fe4c11a59e6aa4d4da
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-06-28 23:16:08 -0400

    better disabled netherite
    TODO: do multi level runes

--- src/main/java/newamazingpvp/lifestealsmp/blacklistener/DisableNetherite.java
+++ src/main/java/newamazingpvp/lifestealsmp/blacklistener/DisableNetherite.java
@@ -18,10 +18,24 @@ public class DisableNetherite implements Listener {
     public void onArmorChange(PlayerArmorChangeEvent event) {
         Player player = event.getPlayer();
         ItemStack newArmorPiece = event.getNewItem();
+        ItemStack oldArmorPiece = event.getOldItem();
 
-        if (newArmorPiece != null && isElytra(newArmorPiece)) {
-            player.getInventory().setChestplate(null);
-            Bukkit.getScheduler().runTaskLater(lifestealSmp, () -> addItemOrDrop(player, newArmorPiece, "Your inventory was full, item dropped"), 20);
+        if (isElytra(oldArmorPiece) || isElytra(newArmorPiece)) {
+            if (player.getInventory().getHelmet() != null && player.getInventory().getHelmet().getType() == Material.NETHERITE_HELMET) {
+                player.getInventory().setHelmet(null);
+            }
+
+            if (player.getInventory().getChestplate() != null && player.getInventory().getChestplate().getType() == Material.NETHERITE_CHESTPLATE) {
+                player.getInventory().setChestplate(null);
+            }
+
+            if (player.getInventory().getLeggings() != null && player.getInventory().getLeggings().getType() == Material.NETHERITE_LEGGINGS) {
+                player.getInventory().setLeggings(null);
+            }
+
+            if (player.getInventory().getBoots() != null && player.getInventory().getBoots().getType() == Material.NETHERITE_BOOTS) {
+                player.getInventory().setBoots(null);
+            }
             player.sendMessage(ChatColor.RED + "Why did you try breaking a rule and wasting resources!? Do /rules");
         }
     }
