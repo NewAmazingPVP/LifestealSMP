a499e83b94b6b764790372233b9ebe83258120a9
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-07-03 17:31:37 -0400

    1

--- src/main/java/newamazingpvp/lifestealsmp/endfight/GeneralBossLieteners/MiningListeners.java
+++ src/main/java/newamazingpvp/lifestealsmp/endfight/GeneralBossLieteners/MiningListeners.java
@@ -1,5 +1,6 @@
 package newamazingpvp.lifestealsmp.endfight.GeneralBossLieteners;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -7,8 +8,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import static newamazingpvp.lifestealsmp.LifestealSMP.SMPworld;
 import static newamazingpvp.lifestealsmp.LifestealSMP.lifestealSmp;
 
 public class MiningListeners implements Listener {
@@ -18,40 +22,44 @@ public class MiningListeners implements Listener {
         Player player = e.getPlayer();
         Block block = e.getBlock();
         Location loc = block.getLocation();
+        Location locSpawn = loc;
+        locSpawn.setY(loc.getY() + 1);
+
+        ItemStack visualItem = new ItemStack(Material.MAGMA_CREAM);
 
 
         if (block.getType() == Material.END_STONE) {
 
+            block.setType(Material.BEDROCK);
+
             if (Math.random() < 0.5) {
-                block.setType(Material.CHEST);
-            } else {
-                block.setType(Material.BEDROCK);
+
+
+                SMPworld.dropItemNaturally(loc, visualItem);
+
+
+
+
+
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+
+                        if (block.getType() == Material.BEDROCK || block.getType() == Material.CHEST) {
+                            block.setType(Material.END_STONE);
+                        }
+
+
+                    }
+                }.runTaskLater(lifestealSmp, 200);
+
+                e.setCancelled(true);
+
             }
 
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-
-                    if(block.getType()==Material.BEDROCK || block.getType()==Material.CHEST){
-                        block.setType(Material.END_STONE);
-                    }
-
-
-                }
-            }.runTaskLater(lifestealSmp, 200);
-
-            e.setCancelled(true);
 
         }
 
 
-
     }
-
-
-
-
-
-
-
 }
