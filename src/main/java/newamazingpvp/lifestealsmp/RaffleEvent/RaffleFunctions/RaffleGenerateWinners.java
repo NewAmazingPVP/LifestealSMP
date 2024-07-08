813aca74a2e92b234a0cb1d3309378dd81047611
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-07-08 15:01:33 -0400

    Update RaffleGenerateWinners.java

--- src/main/java/newamazingpvp/lifestealsmp/RaffleEvent/RaffleFunctions/RaffleGenerateWinners.java
+++ src/main/java/newamazingpvp/lifestealsmp/RaffleEvent/RaffleFunctions/RaffleGenerateWinners.java
@@ -1,7 +1,31 @@
 package newamazingpvp.lifestealsmp.RaffleEvent.RaffleFunctions;
 
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static newamazingpvp.lifestealsmp.LifestealSMP.lifestealSmp;
+
 public class RaffleGenerateWinners {
 
-    
+    //public static Player raffleWinner1 = null;
+    //public static Player raffleWinner2 = null;
+    //public static Player raffleWinner3 = null;
+
+    private static final List<String> testWinnersREMOVE_THIS = List.of(ChatColor.AQUA + "John Doe", ChatColor.GOLD + "Comet99", ChatColor.GRAY + "NewAmazingPVP", ChatColor.GREEN + "Larry");
+
+
+    public static void generateRaffleWinners(){
+
+    }
+
+    public static void showRaffleWinnerAnimation(){
+
+        Bukkit.getScheduler().runTaskLater(lifestealSmp, () -> Bukkit.getOnlinePlayers().forEach(p -> p.sendTitle(org.bukkit.ChatColor.DARK_PURPLE + "" + org.bukkit.ChatColor.BOLD + "Death Beacon!", org.bukkit.ChatColor.RED + "" + org.bukkit.ChatColor.BOLD + ">30sec To Break<", 10, 40, 10)), 5);
+
+    }
 
 }
