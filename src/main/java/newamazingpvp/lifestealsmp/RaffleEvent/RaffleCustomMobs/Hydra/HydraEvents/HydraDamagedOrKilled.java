83eef205b7f3b6c04f496e16310ed2bc1e9d80f1
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-07-11 16:09:08 -0400

    1

---
+++ src/main/java/newamazingpvp/lifestealsmp/RaffleEvent/RaffleCustomMobs/Hydra/HydraEvents/HydraDamagedOrKilled.java
@@ -0,0 +1,37 @@
+package newamazingpvp.lifestealsmp.RaffleEvent.RaffleCustomMobs.Hydra.HydraEvents;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class HydraDamagedOrKilled implements Listener {
+
+    @EventHandler
+    public void whenPlayerHit(EntityDamageByEntityEvent e) {
+
+
+        Entity damagedEntity = e.getEntity();
+        Location loc = damagedEntity.getLocation();
+        Player player = (Player) e.getDamager();
+
+
+        if (damagedEntity.hasMetadata("hydra_mob")) {
+
+            player.sendMessage("Is Hydra");
+
+        }
+
+        if (damagedEntity.hasMetadata("hydra_mob_charged")) {
+
+            player.sendMessage("Is Hydra and is charged");
+
+        }
+
+    }
+}
