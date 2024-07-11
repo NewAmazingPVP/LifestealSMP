994751a1ede91d2bf401528061946460fb8f1b2e
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-07-11 17:59:45 -0400

    own cooldown each event

--- src/main/java/newamazingpvp/lifestealsmp/events/EventsHandler.java
+++ src/main/java/newamazingpvp/lifestealsmp/events/EventsHandler.java
@@ -2,6 +2,7 @@ package newamazingpvp.lifestealsmp.events;
 
 import org.bukkit.event.Listener;
 import org.bukkit.scheduler.BukkitRunnable;
+import newamazingpvp.lifestealsmp.utility.CooldownManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,43 +12,50 @@ import static newamazingpvp.lifestealsmp.utility.TimeManager.*;
 
 public class EventsHandler implements Listener {
     public static final List<Event> events = new ArrayList<>();
-    public EventsHandler(){
-        //events.add(new CustomItemsAndRunes());
-        //events.add(new TrackingDay(SEASON_START_TIME.plusDays(2), 1));
+
+    public EventsHandler() {
+        // events.add(new CustomItemsAndRunes());
+        // events.add(new TrackingDay(SEASON_START_TIME.plusDays(2), 1));
         events.add(new UHCPvPEvent(SEASON_START_TIME.plusDays(19).plusHours(5).plusMinutes(1)));
+
         new BukkitRunnable() {
             @Override
             public void run() {
-                for(Event e : events){
-                    if (eventCooldown.isOnCooldown()) return;
-                    if(isEventInProgress(e)){
+                for (Event e : events) {
+                    CooldownManager eventCooldown = e.getCooldownManager();
+
+                    if (eventCooldown.isOnCooldown()) {
+                        continue;
+                    }
+
+                    if (isEventInProgress(e)) {
                         e.runContinuously();
                         eventCooldown.setCooldown(70);
-                    } else if(isTimePassed(e.getStartTime().minusDays(3)) &&
-                        !isTimePassed(e.getStartTime().minusDays(2).minusHours(23).minusMinutes(59))){
+                    } else if (isTimePassed(e.getStartTime().minusDays(3)) &&
+                            !isTimePassed(e.getStartTime().minusDays(2).minusHours(23).minusMinutes(59))) {
                         e.doWarning();
                         eventCooldown.setCooldown(70);
-                    } else if(isTimePassed(e.getStartTime().minusDays(1)) &&
+                    } else if (isTimePassed(e.getStartTime().minusDays(1)) &&
                             !isTimePassed(e.getStartTime().minusHours(23).minusMinutes(59))) {
                         e.doWarning();
                         eventCooldown.setCooldown(70);
-                    } else if(isTimePassed(e.getStartTime().minusHours(4)) &&
+                    } else if (isTimePassed(e.getStartTime().minusHours(4)) &&
                             !isTimePassed(e.getStartTime().minusHours(3).minusMinutes(59))) {
                         e.doWarning();
                         eventCooldown.setCooldown(70);
-                    } else if(isTimePassed(e.getStartTime().minusHours(1)) &&
+                    } else if (isTimePassed(e.getStartTime().minusHours(1)) &&
                             !isTimePassed(e.getStartTime().minusHours(0).minusMinutes(59))) {
                         e.doWarning();
                         eventCooldown.setCooldown(70);
-                    } else if(isTimePassed(e.getStartTime().minusMinutes(10)) &&
+                    } else if (isTimePassed(e.getStartTime().minusMinutes(10)) &&
                             !isTimePassed(e.getStartTime().minusMinutes(9))) {
                         e.doWarning();
                         eventCooldown.setCooldown(70);
-                    } else if(isTimePassed(e.getStartTime()) &&
+                    } else if (isTimePassed(e.getStartTime()) &&
                             !isTimePassed(e.getStartTime().plusMinutes(5))) {
                         e.onEventStart();
                         eventCooldown.setCooldown(350);
-                    } else if(isTimePassed(e.getEndTime()) &&
+                    } else if (isTimePassed(e.getEndTime()) &&
                             !isTimePassed(e.getEndTime().plusMinutes(5))) {
                         if (!(e.getType() == EventType.ONETIME)) {
                             e.onEventEnd();
@@ -57,11 +65,9 @@ public class EventsHandler implements Listener {
                 }
             }
         }.runTaskTimer(lifestealSmp, 20L, 20L);
-
     }
 
-    public boolean isEventInProgress(Event e){
+    public boolean isEventInProgress(Event e) {
         return isTimePassed(e.getStartTime()) && !isTimePassed(e.getEndTime());
     }
-
 }
