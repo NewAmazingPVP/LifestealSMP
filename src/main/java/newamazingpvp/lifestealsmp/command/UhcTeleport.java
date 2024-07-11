d1bf2c90b2b95aebb1d5873050929cc4f5007054
NewAmazingPVP <gamingofpat@gmail.com>
2024-07-11 17:03:34 -0400

    events implemented
    events implemented

---
+++ src/main/java/newamazingpvp/lifestealsmp/command/UhcTeleport.java
@@ -0,0 +1,34 @@
+package newamazingpvp.lifestealsmp.command;
+
+import newamazingpvp.lifestealsmp.events.UHCPvPEvent;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import static newamazingpvp.lifestealsmp.events.EventsHandler.events;
+import static newamazingpvp.lifestealsmp.events.UHCPvPEvent.*;
+import static newamazingpvp.lifestealsmp.game.CombatLog.isInCombat;
+
+public class UhcTeleport implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+                if(isUhcEvent && !isInCombat(player)) {
+                    if (args.length > 0 && args[0].equalsIgnoreCase("back")) {
+                        teleportBack(player);
+                    } else {
+                        teleportToPvPWorld(player);
+                    }
+                return true;
+            } else {
+                player.sendMessage("No UHC PvP event is currently running.");
+                return true;
+            }
+        } else {
+            sender.sendMessage("Only players can use this command.");
+            return true;
+        }
+    }
+}
