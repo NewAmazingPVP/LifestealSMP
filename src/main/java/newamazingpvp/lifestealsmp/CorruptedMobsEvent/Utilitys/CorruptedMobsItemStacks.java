41ecfb9f1a8a9e26de6ad09ebf727a7af4216708
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-07-12 18:19:59 -0400

    1

--- src/main/java/newamazingpvp/lifestealsmp/CorruptedMobsEvent/Utilitys/CorruptedMobsItemStacks.java
+++ src/main/java/newamazingpvp/lifestealsmp/CorruptedMobsEvent/Utilitys/CorruptedMobsItemStacks.java
@@ -13,7 +13,7 @@ import static newamazingpvp.lifestealsmp.unused.endfight.custommobs.PublicMobMet
 
 public class CorruptedMobsItemStacks {
 
-    private static ItemStack corruptedCoreT1 () {
+    public static ItemStack corruptedCoreT1 () {
 
         PlayerProfile profile = getProfile("https://textures.minecraft.net/texture/9e95293acbcd4f55faf5947bfc5135038b275a7ab81087341b9ec6e453e839");
         ItemStack montuHelm = new ItemStack(Material.PLAYER_HEAD);
@@ -31,9 +31,9 @@ public class CorruptedMobsItemStacks {
     }
 
 
-    private static ItemStack corruptedCoreT2 () {
+    public static ItemStack corruptedCoreT2 () {
 
-        PlayerProfile profile = getProfile("https://textures.minecraft.net/texture/b6a77689a73f39cd9c5f56c8e002ed7c76ea4a905c56a767adfd83cb2ea1f2c4");
+        PlayerProfile profile = getProfile("https://textures.minecraft.net/texture/14d844fee24d5f27ddb669438528d83b684d901b75a6889fe7488dfc4cf7a1c");
         ItemStack montuHelm = new ItemStack(Material.PLAYER_HEAD);
         SkullMeta meta = (SkullMeta) montuHelm.getItemMeta();
         meta.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Corrupted Core" + ChatColor.AQUA + "" + ChatColor.ITALIC + "" + ChatColor.BOLD + " II");
@@ -48,9 +48,9 @@ public class CorruptedMobsItemStacks {
         return montuHelm;
     }
 
-    private static ItemStack corruptedCoreT3 () {
+    public static ItemStack corruptedCoreT3 () {
 
-        PlayerProfile profile = getProfile("https://textures.minecraft.net/texture/b6a77689a73f39cd9c5f56c8e002ed7c76ea4a905c56a767adfd83cb2ea1f2c4");
+        PlayerProfile profile = getProfile("https://textures.minecraft.net/texture/cbfb41f866e7e8e593659986c9d6e88cd37677b3f7bd44253e5871e66d1d424");
         ItemStack montuHelm = new ItemStack(Material.PLAYER_HEAD);
         SkullMeta meta = (SkullMeta) montuHelm.getItemMeta();
         meta.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Corrupted Core" + ChatColor.AQUA + "" + ChatColor.ITALIC + "" + ChatColor.BOLD + " III");
@@ -65,9 +65,9 @@ public class CorruptedMobsItemStacks {
         return montuHelm;
     }
 
-    private static ItemStack corruptedCoreT4 () {
+    public static ItemStack corruptedCoreT4 () {
 
-        PlayerProfile profile = getProfile("https://textures.minecraft.net/texture/b6a77689a73f39cd9c5f56c8e002ed7c76ea4a905c56a767adfd83cb2ea1f2c4");
+        PlayerProfile profile = getProfile("https://textures.minecraft.net/texture/dc9365642c6eddcfedf5b5e14e2bc71257d9e4a3363d123c6f33c55cafbf6d");
         ItemStack montuHelm = new ItemStack(Material.PLAYER_HEAD);
         SkullMeta meta = (SkullMeta) montuHelm.getItemMeta();
         meta.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Corrupted Core" + ChatColor.AQUA + "" + ChatColor.ITALIC + "" + ChatColor.BOLD + " IV");
@@ -82,9 +82,9 @@ public class CorruptedMobsItemStacks {
         return montuHelm;
     }
 
-    private static ItemStack corruptedCoreT5 () {
+    public static ItemStack corruptedCoreT5 () {
 
-        PlayerProfile profile = getProfile("https://textures.minecraft.net/texture/b6a77689a73f39cd9c5f56c8e002ed7c76ea4a905c56a767adfd83cb2ea1f2c4");
+        PlayerProfile profile = getProfile("https://textures.minecraft.net/texture/163bcaf6d2679d8d7d9bf6a474a48a77a8e91747a1084c09256ebc86cb74811");
         ItemStack montuHelm = new ItemStack(Material.PLAYER_HEAD);
         SkullMeta meta = (SkullMeta) montuHelm.getItemMeta();
         meta.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Corrupted Core" + ChatColor.AQUA + "" + ChatColor.ITALIC + "" + ChatColor.BOLD + " V");
