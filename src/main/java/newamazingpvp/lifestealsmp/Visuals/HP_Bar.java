e18fabc7b099795d45e45c74060c0e6d717c9bf3
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-07-13 21:58:01 -0400

    1

--- src/main/java/newamazingpvp/lifestealsmp/Visuals/HP_Bar.java
+++ src/main/java/newamazingpvp/lifestealsmp/Visuals/HP_Bar.java
@@ -3,10 +3,13 @@ package newamazingpvp.lifestealsmp.Visuals;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -22,86 +25,42 @@ import static newamazingpvp.lifestealsmp.LifestealSMP.lifestealSmp;
 
 public class HP_Bar implements Listener {
 
-    private static HashMap<UUID, BossBar> linkHPBossBarToPlayer = new HashMap<>();
-
-    public static BukkitRunnable removeBossBarHP;
 
     @EventHandler
     public void entityDamaged(EntityDamageByEntityEvent e) {
 
-        Entity entity = e.getEntity();
+        LivingEntity damagedEntity = (LivingEntity) e.getEntity();
         Entity attacker = e.getDamager();
         double damageAmount = e.getDamage();
-
-        if (attacker instanceof Player) {
-            UUID testUUID= attacker.getUniqueId();
-
-            BossBar curentBossbar = linkHPBossBarToPlayer.get(testUUID);
-
-            curentBossbar.setTitle(ChatColor.BOLD + "" + ChatColor.RED + "test");
-
-            curentBossbar.setVisible(true);
-
-            removeBossBarHP = new BukkitRunnable() {
-                @Override
-                public void run() {
-
-                    int removeTime = 5;
-
-                    if (removeTime <= 0) {
-                        curentBossbar.setVisible(false);
-                    } else {
-                        removeTime -= 1;
-
-                    }
-                }
-            };
-            removeBossBarHP.runTaskTimer(lifestealSmp, 0L, 20L); // Start immediately and repeat every second
-
-        }
+        double maxHealth = damagedEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+        double currentHealth = maxHealth - e.getFinalDamage();
 
 
 
-
-
+        if(attacker instanceof Player){
+            BossBar HPBar = Bukkit.createBossBar(" ", BarColor.RED, BarStyle.SEGMENTED_20);
+            HPBar.addPlayer((Player) attacker);
+            HPBar.setProgress(1.0);
+            HPBar.setProgress(currentHealth/maxHealth);
+            HPBar.setVisible(true);
+            double hpPercent = currentHealth / maxHealth;
+            if( hpPercent > 0.75 ){
+                HPBar.setTitle(ChatColor.BOLD + "" + ChatColor.DARK_RED + currentHealth + " / " + maxHealth + ChatColor.DARK_RED + "❤");
+            }else if( hpPercent <= 0.75 && hpPercent > 0.50){
+                HPBar.setTitle(ChatColor.BOLD + "" + ChatColor.GOLD + currentHealth + " / " + maxHealth + ChatColor.GOLD + "❤");
+            }else if( hpPercent <= 0.50 && hpPercent > 0.25){
+                HPBar.setTitle(ChatColor.BOLD + "" + ChatColor.YELLOW + currentHealth + " / " + maxHealth + ChatColor.YELLOW + "❤");
+            }else if( hpPercent <= 0.50 && hpPercent > 0){
+                HPBar.setTitle(ChatColor.BOLD + "" + ChatColor.DARK_GREEN + currentHealth + " / " + maxHealth + ChatColor.DARK_GREEN + "❤");
+            }else if(hpPercent <=0 ){
+                HPBar.setVisible(false); 
+            }
 
 
         }
 
 
 
-
-
-
-
-
-
-    @EventHandler
-    public void playerJoin(PlayerJoinEvent e){
-
-        Player player = e.getPlayer();
-        UUID uuid = player.getUniqueId();
-
-        if (!isPlayerInHashMap(uuid)) {
-            addBossBar(player);
-        }
-
-
-    }
-
-
-    private static boolean isPlayerInHashMap(UUID uuid) {
-        return linkHPBossBarToPlayer.containsKey(uuid);
-    }
-
-
-
-
-    private static void addBossBar(Player player) {
-        BossBar bossBar = Bukkit.createBossBar("HP TEST", BarColor.RED, BarStyle.SEGMENTED_20);
-        linkHPBossBarToPlayer.put(player.getUniqueId(), bossBar);
-        bossBar.setVisible(false);
-        bossBar.addPlayer(player);
     }
 
 
