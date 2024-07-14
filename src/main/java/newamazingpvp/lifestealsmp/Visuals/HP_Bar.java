3283bb6a69006942ab0724e7a56ba70322f00f24
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-07-13 23:29:58 -0400

    fix errors
    fix errors

--- src/main/java/newamazingpvp/lifestealsmp/Visuals/HP_Bar.java
+++ src/main/java/newamazingpvp/lifestealsmp/Visuals/HP_Bar.java
@@ -12,28 +12,25 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.UUID;
 
-
 public class HP_Bar implements Listener {
 
-
-    private static HashMap<UUID, BossBar> linkHPBossBarToPlayer = new HashMap<>();
+    private static final HashMap<UUID, BossBar> linkHPBossBarToPlayer = new HashMap<>();
 
     @EventHandler
-    public void playerJoin(PlayerJoinEvent e){
+    public void playerJoin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
         UUID uuid = player.getUniqueId();
 
         if (!isPlayerInHashMap(uuid)) {
             addBossBar(player);
         }
-
     }
 
     private static boolean isPlayerInHashMap(UUID uuid) {
@@ -47,58 +44,46 @@ public class HP_Bar implements Listener {
         bossBar.addPlayer(player);
     }
 
-
-
-
-
-
-
-
-
-
     @EventHandler
     public void entityDamaged(EntityDamageByEntityEvent e) {
-
-
-        LivingEntity damagedEntity = (LivingEntity) e.getEntity();
-        Entity attackedMob = e.getEntity();
-        Entity attacker = e.getDamager();
-        double damageAmount = e.getDamage();
-        double maxHealth = damagedEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
-        double currentHealth = maxHealth - e.getFinalDamage();
-        DecimalFormat df = new DecimalFormat("0.0");
-
-
-
-        if(attacker instanceof Player){
-            UUID testUUID= attacker.getUniqueId();
-            BossBar HPBar = linkHPBossBarToPlayer.get(testUUID);
-
-            HPBar.addPlayer((Player) attacker);
-            HPBar.setProgress(1.0);
-            HPBar.setProgress(currentHealth/maxHealth);
-            HPBar.setVisible(true);
-            double hpPercent = currentHealth / maxHealth;
-            if( hpPercent > 0.75 ){
-                HPBar.setTitle(ChatColor.DARK_RED + "" + ChatColor.BOLD + df.format(currentHealth) + " / " + df.format(maxHealth) + " | -" + df.format(damageAmount) + ChatColor.DARK_RED + "❤");
-            }else if( hpPercent <= 0.75 && hpPercent > 0.50){
-                HPBar.setTitle(ChatColor.GOLD + "" + ChatColor.BOLD + df.format(currentHealth) + " / " + df.format(maxHealth) + " | -" + df.format(damageAmount) + ChatColor.GOLD + "❤");
-            }else if( hpPercent <= 0.50 && hpPercent > 0.25){
-                HPBar.setTitle(ChatColor.YELLOW + "" + ChatColor.BOLD + df.format(currentHealth) + " / " + df.format(maxHealth) + " | -" + df.format(damageAmount) + ChatColor.YELLOW + "❤");
-            }else if( hpPercent <= 0.50 && hpPercent > 0){
-                HPBar.setTitle(ChatColor.DARK_GREEN + "" + ChatColor.BOLD + df.format(currentHealth) + " / " + df.format(maxHealth) + " | -" + df.format(damageAmount) + ChatColor.DARK_GREEN + "❤");
-            }else if(attackedMob.isDead()){
-                HPBar.setVisible(false);
-            }
-
-
+        if (!(e.getEntity() instanceof LivingEntity)) {
+            return;
         }
 
+        LivingEntity damagedEntity = (LivingEntity) e.getEntity();
+        Entity attacker = e.getDamager();
 
+        if (!(attacker instanceof Player)) {
+            return;
+        }
 
+        Player playerAttacker = (Player) attacker;
+        UUID attackerUUID = playerAttacker.getUniqueId();
+
+        BossBar HPBar = linkHPBossBarToPlayer.get(attackerUUID);
+        if (HPBar == null) {
+            addBossBar(playerAttacker);
+            HPBar = linkHPBossBarToPlayer.get(attackerUUID);
+        }
+
+        double maxHealth = damagedEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+        double currentHealth = Math.max(damagedEntity.getHealth() - e.getFinalDamage(), 0);
+        DecimalFormat df = new DecimalFormat("0.0");
+        double hpPercent = currentHealth / maxHealth;
+
+        HPBar.setProgress(hpPercent);
+        HPBar.setVisible(true);
+
+        if (hpPercent > 0.75) {
+            HPBar.setTitle(ChatColor.DARK_RED + "" + ChatColor.BOLD + df.format(currentHealth) + " / " + df.format(maxHealth) + " | -" + df.format(e.getDamage()) + ChatColor.DARK_RED + "❤");
+        } else if (hpPercent > 0.50) {
+            HPBar.setTitle(ChatColor.GOLD + "" + ChatColor.BOLD + df.format(currentHealth) + " / " + df.format(maxHealth) + " | -" + df.format(e.getDamage()) + ChatColor.GOLD + "❤");
+        } else if (hpPercent > 0.25) {
+            HPBar.setTitle(ChatColor.YELLOW + "" + ChatColor.BOLD + df.format(currentHealth) + " / " + df.format(maxHealth) + " | -" + df.format(e.getDamage()) + ChatColor.YELLOW + "❤");
+        } else if (hpPercent > 0) {
+            HPBar.setTitle(ChatColor.DARK_GREEN + "" + ChatColor.BOLD + df.format(currentHealth) + " / " + df.format(maxHealth) + " | -" + df.format(e.getDamage()) + ChatColor.DARK_GREEN + "❤");
+        } else {
+            HPBar.setVisible(false);
+        }
     }
-
-
-
-
 }
