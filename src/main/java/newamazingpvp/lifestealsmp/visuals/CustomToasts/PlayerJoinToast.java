65f4b374db99101331a233247998605036bbb5d9
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-08-23 22:17:18 -0400

    Update PlayerJoinToast.java

--- src/main/java/newamazingpvp/lifestealsmp/visuals/CustomToasts/PlayerJoinToast.java
+++ src/main/java/newamazingpvp/lifestealsmp/visuals/CustomToasts/PlayerJoinToast.java
@@ -1,21 +1,46 @@
 package newamazingpvp.lifestealsmp.visuals.CustomToasts;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import static newamazingpvp.lifestealsmp.visuals.CustomToasts.CreateCustomToast.Style.TASK;
+
 public class PlayerJoinToast implements Listener {
+
+    private static String displayMessage = ChatColor.DARK_RED + "[ERROR]";
 
 
     @EventHandler
     public void playerJoinEvent(PlayerJoinEvent e){
         Player player = e.getPlayer();
 
+        String name = player.getName();
+
+        if(name == "Comet99" || name == "Newamazingpvp"){
+            displayMessage = ChatColor.DARK_RED + "" + ChatColor.BOLD + name + "Has Joined " + ChatColor.DARK_GREEN + "" + ChatColor.BOLD + "[+]";
+        }else{
+            displayMessage = ChatColor.BLUE + "" + ChatColor.BOLD + name + "Has Joined " + ChatColor.DARK_GREEN + "" + ChatColor.BOLD + "[+]";
+        }
+
+        final String materialName = "dirt";
+
+        try {
+            Material.valueOf(materialName.toUpperCase());
+
+        } catch (final Throwable t) {
+
+            return;
+        }
 
 
-        //CreateCustomToast.displayTo((Player) sender, materialName, message, style);
+        
+
+        CreateCustomToast.displayTo(materialName, displayMessage, TASK);
 
     }
 
