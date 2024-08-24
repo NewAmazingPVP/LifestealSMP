b448ac743ea56986b3f2c702c8e30a54a4934d75
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-08-24 12:53:26 -0400

    season 14 start
    season 14 start

--- src/main/java/newamazingpvp/lifestealsmp/command/RulesCommand.java
+++ src/main/java/newamazingpvp/lifestealsmp/command/RulesCommand.java
@@ -29,6 +29,7 @@ public class RulesCommand implements CommandExecutor {
             //player.sendMessage(ChatColor.WHITE + "- Do not trick or manipulate new players to hit you for hearts");
             player.sendMessage(ChatColor.WHITE + "- Netherite armor and tools are allowed.");
             player.sendMessage(ChatColor.WHITE + "- Do not lava cast, trap, or grief in the vicinity of spawn.");
+            player.sendMessage(ChatColor.WHITE + "- Do not use the lava bucket or any bypass to kill newbies or respawns as long as they have their newbie or death protection.");
             player.sendMessage(ChatColor.WHITE + "- Using newbie or death protection to grief/steal/abuse etc is not allowed.");
             player.sendMessage(ChatColor.WHITE + "- No offensive/abusive behavior/builds in-game");
             player.sendMessage(ChatColor.WHITE + "- No offensive language/skins/names");
