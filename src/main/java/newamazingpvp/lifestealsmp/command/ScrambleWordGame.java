179af57d0b6a6a75dcf64b4e36c429c66cfe16ae
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-09-01 11:01:34 -0400

    1

--- src/main/java/newamazingpvp/lifestealsmp/command/ScrambleWordGame.java
+++ src/main/java/newamazingpvp/lifestealsmp/command/ScrambleWordGame.java
@@ -6,7 +6,9 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatEvent;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -14,6 +16,9 @@ import java.util.List;
 
 public class ScrambleWordGame implements Listener, CommandExecutor {
 
+    private static boolean isScrambleGameRunning = false;
+
+    private static String word = null;
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
@@ -26,14 +31,13 @@ public class ScrambleWordGame implements Listener, CommandExecutor {
             return false;
         }
 
-        String word = args[0];
+        word = args[0];
 
 
         List<Character> characters = new ArrayList<>();
         for (char c : word.toCharArray()) {
             characters.add(c);
         }
-
         Collections.shuffle(characters);
 
         StringBuilder shuffledString = new StringBuilder();
@@ -42,10 +46,38 @@ public class ScrambleWordGame implements Listener, CommandExecutor {
         }
 
 
-        Bukkit.broadcastMessage(ChatColor.BOLD + "" + ChatColor.AQUA + shuffledString);
 
+        Bukkit.broadcastMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "==========" + ChatColor.DARK_PURPLE + ChatColor.BOLD + " [Word Scramble] " + ChatColor.AQUA + ChatColor.BOLD + "==========");
+        Bukkit.broadcastMessage(" ");
+        Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "Unscramble " +ChatColor.AQUA + "" + ChatColor.BOLD + shuffledString);
+        Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "and say it in chat and win absolutely nothing!" );
+        Bukkit.broadcastMessage(" ");
+        Bukkit.broadcastMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "===============================");
+
+
+        isScrambleGameRunning = true;
 
         return true;
     }
+
+
+    @EventHandler
+    public void playerChatEvent(PlayerChatEvent e){
+
+        if(isScrambleGameRunning){
+            Player player = e.getPlayer();
+            String chatMessage = e.getMessage();
+
+            if(chatMessage.equals(word)){
+                isScrambleGameRunning = false;
+                String name = player.getName();
+                Bukkit.broadcastMessage(ChatColor.GOLD + "" + ChatColor.BOLD + name + " Has guessed it correct!");
+                Bukkit.broadcastMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "The word was " + "[" + word + "]"); 
+            }
+        }
+    }
+
+
+
 
 }
