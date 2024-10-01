df71d7035b6e5015cd27df1c6a707119303b31fc
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-09-30 21:32:30 -0400

    Revert "Merge pull request #2 from NewAmazingPVP/master"
    This reverts commit 4449fa9280c729f3e1cc77c6849e911fb276b206, reversing
    changes made to a967c09806fb1b9aeac3a969ca7f11b6c1c7dddc.

--- src/main/java/newamazingpvp/lifestealsmp/command/TournamentRegister.java
+++ src/main/java/newamazingpvp/lifestealsmp/command/TournamentRegister.java
@@ -1,6 +1,5 @@
 package newamazingpvp.lifestealsmp.command;
 
-import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,8 +9,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.time.ZonedDateTime;
 
-import static newamazingpvp.lifestealsmp.events.TimeManager.SEASON_START_TIME;
-import static newamazingpvp.lifestealsmp.events.TimeManager.isTimePassed;
 import static newamazingpvp.lifestealsmp.events.TournamentEvent.isTournamentEvent;
 import static newamazingpvp.lifestealsmp.events.TournamentEvent.registerPlayer;
 import static newamazingpvp.lifestealsmp.events.UHCPvPEvent.isUhcEvent;
@@ -22,17 +19,10 @@ public class TournamentRegister implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!isTournamentEvent) {
-            if(isTimePassed(SEASON_START_TIME.plusDays(6).plusHours(14)) && !isTimePassed(SEASON_START_TIME.plusDays(7).plusHours(4))) {
-                if (sender instanceof Player player) {
-                    registerPlayer(player);
-                    player.sendMessage("Registered for 1v1 tournament event!");
-                    return true;
-                }
-            } else {
-                sender.sendMessage(ChatColor.RED + "You can only register for the tournament event on the day of the event.");
+            if (sender instanceof Player player) {
+                registerPlayer(player);
+                return true;
             }
-        } else {
-            sender.sendMessage("Tournament event is already in progress! Next time register before the event.");
         }
         return false;
     }
--- src/main/java/newamazingpvp/lifestealsmp/listener/CombatLogListener.java
+++ src/main/java/newamazingpvp/lifestealsmp/listener/CombatLogListener.java
@@ -6,8 +6,10 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.damage.DamageSource;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -18,11 +20,13 @@ import org.bukkit.scheduler.BukkitRunnable;
 import java.util.List;
 
 import static newamazingpvp.lifestealsmp.LifestealSMP.lifestealSmp;
+import static newamazingpvp.lifestealsmp.allyteams.TeamsManager.onSameTeam;
 import static newamazingpvp.lifestealsmp.customitems.utils.ItemStacks.extraHeart;
 import static newamazingpvp.lifestealsmp.game.CombatLog.*;
 import static newamazingpvp.lifestealsmp.game.Compass.getPlaytime;
 import static newamazingpvp.lifestealsmp.listener.CombatProtectionHandler.heartCooldownPlayers;
 import static newamazingpvp.lifestealsmp.listener.CombatProtectionHandler.invincibilityPlayers;
+import static newamazingpvp.lifestealsmp.utility.Utils.returnPlayerDamager;
 import static newamazingpvp.lifestealsmp.variables.Misc.maxHp;
 
 public class CombatLogListener implements Listener {
@@ -33,12 +37,29 @@ public class CombatLogListener implements Listener {
     }
 
     @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if (event.getEntity() instanceof Player damaged) {
+            Player damager = returnPlayerDamager(event.getDamager());
+            if (damager == null) return;
+            if (onSameTeam(damaged, damager)) {
+                event.setCancelled(true);
+                return;
+            }
+            if (!event.isCancelled()) {
+                if (damaged.equals(damager)) return;
+                tagPlayer(damager, damaged);
+                tagPlayer(damaged, damager);
+            }
+        }
+    }
+
+    @EventHandler
     public void onPlayerDisconnect(PlayerQuitEvent e) {
         if (isInCombat(e.getPlayer())) {
             Player p = e.getPlayer();
 
             if (getCombatTimer(p) < 85) {
-                if (!heartCooldownPlayers.contains(e.getPlayer().getName()) && getPlaytime(p) > 144000) {
+                /*if (!heartCooldownPlayers.contains(e.getPlayer().getName()) && getPlaytime(p) > 144000) {
                     p.setMaxHealth(p.getMaxHealth() - 2);
                     Player winner = getEnemies(p).get(getEnemies(p).size() - 1);
                     if (!(winner.getMaxHealth() > maxHp)) {
@@ -53,7 +74,7 @@ public class CombatLogListener implements Listener {
                             winner.sendMessage(ChatColor.LIGHT_PURPLE + "Heart was dropped because your inventory was full");
                         }
                     }
-                }
+                }*/
             }
             p.setHealth(0.0);
 
@@ -67,8 +88,10 @@ public class CombatLogListener implements Listener {
             }
 
             String deathMessage = p.getName() + " was killed instantly due to logging out during combat!";
+            // fix the code below it gives errors
             PlayerDeathEvent deathEvent = new PlayerDeathEvent(p, (DamageSource) getEnemies(p).get(0), List.of(inventoryContents), 0, 0, 0, 0, deathMessage);
             Bukkit.getPluginManager().callEvent(deathEvent);
+            /*
             heartCooldownPlayers.add(p.getName());
             invincibilityPlayers.add(p.getName());
             new BukkitRunnable() {
@@ -83,7 +106,7 @@ public class CombatLogListener implements Listener {
                     invincibilityPlayers.remove(p.getName());
                 }
             }.runTaskLater(lifestealSmp, 20 * 60 * 15);
-
+            */
             cancelCombatData(e.getPlayer());
             removeEnemies(e.getPlayer());
         }
