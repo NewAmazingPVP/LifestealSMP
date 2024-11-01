2090b68e0299133f696ba2da8cd0f0b5a940ee34
NewAmazingPVP <gamingofpat@gmail.com>
2024-10-31 20:34:59 -0400

    potentially fix end fight for the final time
    potentially fix end fight for the final time

--- src/main/java/newamazingpvp/lifestealsmp/command/StartEndFight.java
+++ src/main/java/newamazingpvp/lifestealsmp/command/StartEndFight.java
@@ -31,98 +31,102 @@ public class StartEndFight implements CommandExecutor {
         worldBorder.setCenter(0, 0);
         worldBorder.setSize(310);
 
-        if (isEndFightEnabled) {
-            new BukkitRunnable() {
-                int minutesPassed = 0;
-
-                @Override
-                public void run() {
-                    if (!isEndFightEnabled) {
-                        this.cancel();
-                        return;
-                    }
-                    //if minutes passed is between 30 and 33 then do below but dont this.cancel and if greater than 33 then just apply poison if in survival
-
-                    if (minutesPassed >= 30 && minutesPassed < 33) {
-                        //this.cancel();
-                        worldBorder.setSize(1);
-                        Bukkit.broadcastMessage(ChatColor.BOLD + "" + ChatColor.DARK_PURPLE + "Border is now 1 block wide. Going outside border may instant kill you");
-                        worldBorder.setDamageBuffer(0);
-                        worldBorder.setDamageAmount(4);
-                        worldBorder.setWarningDistance(10);
-                        for (Player p : Bukkit.getOnlinePlayers()) {
-                            if (p.getGameMode() == GameMode.SURVIVAL) {
-                                p.addPotionEffect(new PotionEffect(PotionEffectType.POISON, Integer.MAX_VALUE, 2, true, false));
-                                p.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, Integer.MAX_VALUE, 1, true, false));
-                                p.sendTitle(ChatColor.RED + "WARNING!", ChatColor.YELLOW + "Fight to the death, or get eliminated by poison!");
-                            }
-                        }
-                        Bukkit.broadcastMessage(ChatColor.DARK_RED + "Fight to the death, or get eliminated by instant harming!");
-                        return;
-                    } else if (minutesPassed >= 33) {
-                        //this.cancel();
-                        //worldBorder.setSize(1);
-                        //Bukkit.broadcastMessage(ChatColor.BOLD + "" + ChatColor.DARK_PURPLE + "Border is now 1 block wide. Going outside border may instant kill you");
-                        //worldBorder.setDamageBuffer(0);
-                        //worldBorder.setDamageAmount(4);
-                        //.setWarningDistance(10);
-                        for (Player p : Bukkit.getOnlinePlayers()) {
-                            if (p.getGameMode() == GameMode.SURVIVAL) {
-                                p.addPotionEffect(new PotionEffect(PotionEffectType.POISON, Integer.MAX_VALUE, 2, true, false));
-                                p.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, Integer.MAX_VALUE, 1, true, false));
-                                p.sendTitle(ChatColor.RED + "WARNING!", ChatColor.YELLOW + "Fight to the death, or get eliminated by poison!");
-                            }
-                        }
-                        Bukkit.broadcastMessage(ChatColor.DARK_RED + "Fight to the death, or get eliminated by instant harming!");
-                        return;
-                    }
-
-                    double newSize = worldBorder.getSize() - 10;
-                    Bukkit.broadcastMessage(ChatColor.BOLD + "" + ChatColor.DARK_PURPLE + "Border is now " + newSize + " blocks!");
-                    worldBorder.setSize(newSize);
-                    minutesPassed++;
+        World finalWorld = world;
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (!isEndFightEnabled) {
+                    this.cancel();
+                    return;
                 }
-            }.runTaskTimer(lifestealSmp, 1200, 20 * 60);
-
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    if (!isEndFightEnabled) {
-                        this.cancel();
-                        return;
-                    }
-
-                    for (Player p : lifestealSmp.getServer().getOnlinePlayers()) {
-                        p.teleport(endFightSpawn);
-                        p.setInvulnerable(true);
-
-                        getServer().getScheduler().runTaskLater(lifestealSmp, () -> p.setInvulnerable(false), 20 * 60);
-
-                        p.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "GET READY!" + ChatColor.YELLOW + " The end fight will be starting in 60 seconds!");
-                        endFightParticipants.add(p.getName());
-                    }
-
-                    Bukkit.getScheduler().runTaskLater(lifestealSmp, () ->
-                            lifestealSmp.getServer().broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "GET READY!" + ChatColor.YELLOW + " The end fight will be starting in 30 seconds!"), 600);
-
-                    int delay = 1000;
-                    for (int i = 10; i > 0; i--) {
-                        final int count = i;
-                        Bukkit.getScheduler().runTaskLater(lifestealSmp, () ->
-                                lifestealSmp.getServer().broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "GET READY!" + ChatColor.YELLOW + " The end fight will be starting in " + count + " seconds!"), delay);
-                        delay += 20;
-                    }
-
-                    Bukkit.getScheduler().runTaskLater(lifestealSmp, () ->
-                            lifestealSmp.getServer().broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "GO!"), 1200);
-
-                    for (Player p : lifestealSmp.getServer().getOnlinePlayers()) {
-                        Bukkit.getScheduler().runTaskLater(lifestealSmp, () ->
-                                p.sendTitle(ChatColor.RED + "" + ChatColor.BOLD + "GO!", ChatColor.GOLD + "Good Luck!"), 1200);
-                    }
+                if (finalWorld.getWorldType() == null || finalWorld.getEnvironment() == null || Bukkit.getWorld(finalWorld.getName()) == null) {
+                    return;
                 }
-            }.runTaskLater(lifestealSmp, 500L);
-        }
+
+                for (Player p : lifestealSmp.getServer().getOnlinePlayers()) {
+                    p.teleport(endFightSpawn);
+                    p.setInvulnerable(true);
+
+                    getServer().getScheduler().runTaskLater(lifestealSmp, () -> p.setInvulnerable(false), 20 * 60);
+
+                    p.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "GET READY!" + ChatColor.YELLOW + " The end fight will be starting in 60 seconds!");
+                    endFightParticipants.add(p.getName());
+                }
+
+                Bukkit.getScheduler().runTaskLater(lifestealSmp, () ->
+                        lifestealSmp.getServer().broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "GET READY!" + ChatColor.YELLOW + " The end fight will be starting in 30 seconds!"), 600);
+
+                int delay = 1000;
+                for (int i = 10; i > 0; i--) {
+                    final int count = i;
+                    Bukkit.getScheduler().runTaskLater(lifestealSmp, () ->
+                            lifestealSmp.getServer().broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "GET READY!" + ChatColor.YELLOW + " The end fight will be starting in " + count + " seconds!"), delay);
+                    delay += 20;
+                }
+
+                Bukkit.getScheduler().runTaskLater(lifestealSmp, () ->
+                        lifestealSmp.getServer().broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "GO!"), 1200);
+
+                for (Player p : lifestealSmp.getServer().getOnlinePlayers()) {
+                    Bukkit.getScheduler().runTaskLater(lifestealSmp, () ->
+                            p.sendTitle(ChatColor.RED + "" + ChatColor.BOLD + "GO!", ChatColor.GOLD + "Good Luck!"), 1200);
+                }
+                if (isEndFightEnabled) {
+                    new BukkitRunnable() {
+                        int minutesPassed = 0;
+
+                        @Override
+                        public void run() {
+                            if (!isEndFightEnabled) {
+                                this.cancel();
+                                return;
+                            }
+                            //if minutes passed is between 30 and 33 then do below but dont this.cancel and if greater than 33 then just apply poison if in survival
+
+                            if (minutesPassed >= 30 && minutesPassed < 33) {
+                                //this.cancel();
+                                worldBorder.setSize(1);
+                                Bukkit.broadcastMessage(ChatColor.BOLD + "" + ChatColor.DARK_PURPLE + "Border is now 1 block wide. Going outside border may instant kill you");
+                                worldBorder.setDamageBuffer(0);
+                                worldBorder.setDamageAmount(4);
+                                worldBorder.setWarningDistance(10);
+                                for (Player p : Bukkit.getOnlinePlayers()) {
+                                    if (p.getGameMode() == GameMode.SURVIVAL) {
+                                        p.addPotionEffect(new PotionEffect(PotionEffectType.POISON, Integer.MAX_VALUE, 2, true, false));
+                                        p.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, Integer.MAX_VALUE, 1, true, false));
+                                        p.sendTitle(ChatColor.RED + "WARNING!", ChatColor.YELLOW + "Fight to the death, or get eliminated by poison!");
+                                    }
+                                }
+                                Bukkit.broadcastMessage(ChatColor.DARK_RED + "Fight to the death, or get eliminated by instant harming!");
+                                return;
+                            } else if (minutesPassed >= 33) {
+                                //this.cancel();
+                                //worldBorder.setSize(1);
+                                //Bukkit.broadcastMessage(ChatColor.BOLD + "" + ChatColor.DARK_PURPLE + "Border is now 1 block wide. Going outside border may instant kill you");
+                                //worldBorder.setDamageBuffer(0);
+                                //worldBorder.setDamageAmount(4);
+                                //.setWarningDistance(10);
+                                for (Player p : Bukkit.getOnlinePlayers()) {
+                                    if (p.getGameMode() == GameMode.SURVIVAL) {
+                                        p.addPotionEffect(new PotionEffect(PotionEffectType.POISON, Integer.MAX_VALUE, 2, true, false));
+                                        p.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, Integer.MAX_VALUE, 1, true, false));
+                                        p.sendTitle(ChatColor.RED + "WARNING!", ChatColor.YELLOW + "Fight to the death, or get eliminated by poison!");
+                                    }
+                                }
+                                Bukkit.broadcastMessage(ChatColor.DARK_RED + "Fight to the death, or get eliminated by instant harming!");
+                                return;
+                            }
+
+                            double newSize = worldBorder.getSize() - 10;
+                            Bukkit.broadcastMessage(ChatColor.BOLD + "" + ChatColor.DARK_PURPLE + "Border is now " + newSize + " blocks!");
+                            worldBorder.setSize(newSize);
+                            minutesPassed++;
+                        }
+                    }.runTaskTimer(lifestealSmp, 1200, 20 * 60);
+                }
+                this.cancel();
+            }
+        }.runTaskTimer(lifestealSmp, 0L, 20L);
 
         return true;
     }
