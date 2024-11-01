0b6184fdbdd605d5d0d8f5721bf9f4e2f90f9901
NewAmazingPVP <gamingofpat@gmail.com>
2024-10-31 21:39:37 -0400

    fully tested fix for end fight
    fully tested fix for end fight

--- src/main/java/newamazingpvp/lifestealsmp/variables/Loc.java
+++ src/main/java/newamazingpvp/lifestealsmp/variables/Loc.java
@@ -7,9 +7,9 @@ import org.bukkit.World;
 public class Loc {
 
     public static World SMPworld = Bukkit.getWorld("world");
-    public static Location endFightSpawn = new Location(Bukkit.getWorld("end_fight_world"), 25.0, 80.0, 25.0);
-    public static Location endSpawn = new Location(Bukkit.getWorld("end_fight_world"), 0, 60.0, 0);
-    public static Location endPortalCenter = new Location(Bukkit.getWorld("end_fight_world"), 0, 70.0, 0);
+    //public static Location endFightSpawn = new Location(Bukkit.getWorld("end_fight_world"), 25.0, 80.0, 25.0);
+    //public static Location endSpawn = new Location(Bukkit.getWorld("end_fight_world"), 0, 60.0, 0);
+    //public static Location endPortalCenter = new Location(Bukkit.getWorld("end_fight_world"), 0, 70.0, 0);
     public static Location lobby = new Location(Bukkit.getWorld("world"), -20, 270.0, 27);
     public static Location spawnLoc1 = new Location(Bukkit.getWorld("world"), -100, -64, -100);
     public static Location spawnLoc2 = new Location(Bukkit.getWorld("world"), 100, 320, 100);
