4ffba386d67334da1173d798b0e63b8ee12847b0
NewAmazingPVP <gamingofpat@gmail.com>
2024-11-03 21:50:27 -0500

    clarify

--- src/main/java/newamazingpvp/lifestealsmp/events/ServerOpening.java
+++ src/main/java/newamazingpvp/lifestealsmp/events/ServerOpening.java
@@ -64,7 +64,7 @@ public class ServerOpening extends BaseEvent {
     @Override
     public void doWarning() {
         sendDiscordNewsMessage(mcServer + " The new **Lifesteal** season will be released in " + formatDuration(startTime) + " exactly!!!", "1032411739351941120");
-        sendDiscordNewsMessage("## End fight fixed, runes nerfed, tournament event fully added!", "1032411739351941120");
+        //sendDiscordNewsMessage("## End fight fixed, runes nerfed, tournament event fully added!", "1032411739351941120");
         Bukkit.getWorld("world").setSpawnLocation(0, 320, 0);
     }
 
