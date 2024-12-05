5ab622296b2c4d178ed8d14ef92867730b16bc61
NewAmazingPVP <gamingofpat@gmail.com>
2024-12-04 20:02:59 -0500

    disable items in combat
    disable items in combat

--- src/main/java/newamazingpvp/lifestealsmp/LifestealSMP.java
+++ src/main/java/newamazingpvp/lifestealsmp/LifestealSMP.java
@@ -188,6 +188,7 @@ public final class LifestealSMP extends JavaPlugin implements Listener, PluginMe
         getServer().getPluginManager().registerEvents(new DisableCustomItems(), this);
         getServer().getPluginManager().registerEvents(new Disenchant(), this);
         getServer().getPluginManager().registerEvents(new AntiChunkBan(), this);
+        getServer().getPluginManager().registerEvents(new DisableItemsInCombat(), this);
         startTPSTracking();
         getServer().getScheduler().runTaskTimer(this, Utils::adjustPerformance, 120, 1);
         getCommand("trade").setExecutor(new Trade());
