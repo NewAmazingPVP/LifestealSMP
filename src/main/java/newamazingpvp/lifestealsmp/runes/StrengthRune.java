971152ce37ba7249ac06d7acbaa7536e4250625a
NewAmazingPVP <gamingofpat@gmail.com>
2024-12-24 09:55:06 -0500

    nerf strength rune
    nerf strength rune

--- src/main/java/newamazingpvp/lifestealsmp/runes/StrengthRune.java
+++ src/main/java/newamazingpvp/lifestealsmp/runes/StrengthRune.java
@@ -11,8 +11,8 @@ public class StrengthRune extends AbstractRune {
 
     private static final Component name = runeGradient("Strength Rune");
     private static final EntityType mob = EntityType.PIGLIN_BRUTE;
-    private static final PotionEffect effect = new PotionEffect(PotionEffectType.STRENGTH, 200, 0);
-    private static final PotionEffect effectII = new PotionEffect(PotionEffectType.STRENGTH, 200, 1);
+    private static final PotionEffect effect = new PotionEffect(PotionEffectType.STRENGTH, 100, 0);
+    private static final PotionEffect effectII = new PotionEffect(PotionEffectType.STRENGTH, 100, 1);
     private static final String lore = ChatColor.LIGHT_PURPLE + "Grants permanent random duration " + formatEffectKey(effect.getType().getKey().getKey()) + " " + formatAmplifier(effect.getAmplifier());
 
     public StrengthRune() {
