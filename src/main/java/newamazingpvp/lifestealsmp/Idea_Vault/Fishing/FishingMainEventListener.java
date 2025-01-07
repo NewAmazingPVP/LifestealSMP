4d01dfc893887aeef8d0052939c793e84604b7cf
Comet991 <139285587+Comet991@users.noreply.github.com>
2025-01-07 12:29:13 -0500

    Update FishingMainEventListener.java

--- src/main/java/newamazingpvp/lifestealsmp/Idea_Vault/Fishing/FishingMainEventListener.java
+++ src/main/java/newamazingpvp/lifestealsmp/Idea_Vault/Fishing/FishingMainEventListener.java
@@ -1,5 +1,6 @@
 package newamazingpvp.lifestealsmp.Idea_Vault.Fishing;
 
+import com.destroystokyo.paper.event.player.PlayerJumpEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Biome;
 import org.bukkit.entity.Item;
@@ -15,20 +16,21 @@ public class FishingMainEventListener implements Listener {
 
 
     @EventHandler
-    public void playerFishItem(PlayerFishEvent e){
+    public void playerFishItem(PlayerJumpEvent e){
 
         Player player = e.getPlayer();
         Biome b = player.getLocation().getBlock().getBiome();
 
         Bukkit.getServer().broadcastMessage("test");
+        player.getInventory().addItem(generateFishingDrop(b,player));
 
-        if(e.getState().equals(PlayerFishEvent.State.CAUGHT_ENTITY)){
-            if(e.getCaught() instanceof Item){
-                Item stack = (Item) e.getCaught();
-                stack.setItemStack(generateFishingDrop(b,player));
+        //if(e.getState().equals(PlayerFishEvent.State.CAUGHT_ENTITY)){
+            //if(e.getCaught() instanceof Item){
+                //Item stack = (Item) e.getCaught();
+                //stack.setItemStack(generateFishingDrop(b,player));
 
-            }
-        }
+            //}
+        //}
 
 
 
