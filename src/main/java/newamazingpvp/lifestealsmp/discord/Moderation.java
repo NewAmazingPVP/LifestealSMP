b3b7a13f961d300c6dd15ee5764306077e13af7c
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2025-02-02 19:49:48 -0500

    better chat filter and moderation might add GIF/videos moderation
    better chat filter and moderation might add GIF/videos moderation

--- src/main/java/newamazingpvp/lifestealsmp/discord/Moderation.java
+++ src/main/java/newamazingpvp/lifestealsmp/discord/Moderation.java
@@ -1,11 +1,14 @@
 package newamazingpvp.lifestealsmp.discord;
 
+import com.squareup.gifencoder.GifEncoder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
+
+import java.awt.image.BufferedImage;
 
 import static newamazingpvp.lifestealsmp.LifestealSMP.lifestealSmp;
 import static newamazingpvp.lifestealsmp.blacklistener.ChatFilter.*;
@@ -21,21 +24,27 @@ public class Moderation extends ListenerAdapter {
         String messageContent = event.getMessage().getContentRaw();
         String censoredMessage = censorBlacklistedWordsNonLinks(messageContent);
         if (shouldBeWarned(messageContent)) {
-            sendDiscordMessage(event.getAuthor().getName() + "possibly tried saying something bad. Here is the flagged language **" + messageContent + "**", "1019965981025652738");
+            sendDiscordMessage(event.getAuthor().getName() + " possibly tried saying something bad. Here is the flagged language " + messageContent, "1019965981025652738");
         }
         if (!messageContent.equals(censoredMessage)) {
             event.getMessage().delete().queue();
-            sendDiscordMessage(event.getAuthor().getName() + "tried saying something bad. Here is the moderated language **" + messageContent + "**", "1019965981025652738");
+            sendDiscordMessage(event.getAuthor().getName() + " said something bad. Here is the moderated language **" + messageContent + "**", "1019965981025652738");
         }
         if (isFlaggedByModeration(messageContent)) {
             event.getMessage().delete().queue();
-            sendDiscordMessage(event.getAuthor().getName() + "tried saying something bad. Here is the moderated language **" + messageContent + "**", "1019965981025652738");
+            sendDiscordMessage(event.getAuthor().getName() + " said something bad. Here is the moderated language **" + messageContent + "**", "1019965981025652738");
         }
         for (Message.Attachment attachment : event.getMessage().getAttachments()) {
             if (attachment.isImage() && isFlaggedByImageModeration(attachment.getUrl())) {
                 event.getMessage().delete().queue();
                 sendDiscordMessage(event.getAuthor().getName() + " tried sending a flagged image: " + attachment.getUrl(), "1019965981025652738");
                 return;
+            }
+            if (attachment.isVideo() || attachment.isImage()){
+                if (attachment.getFileExtension() != null && attachment.getFileExtension().equalsIgnoreCase("gif")) {
+                    // add gif/video moderation if we need it
+                    // we can use JCodec, TwelveMonkeys ImageIO, or GifDecoder/Encoder
+                }
             }
         }
     }
