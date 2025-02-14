491ed0d87f7b3f3fcc8b2960cfbb172243cc8795
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2025-02-13 22:07:46 -0500

    mace, tems, feather sword, etc changes
    mace, tems, feather sword, etc changes

--- src/main/java/newamazingpvp/lifestealsmp/allyteams/TeamsManager.java
+++ src/main/java/newamazingpvp/lifestealsmp/allyteams/TeamsManager.java
@@ -36,7 +36,7 @@ public class TeamsManager {
                 p.sendMessage(ChatColor.RED + "You this is not the same team as you have been invited to!");
                 return;
             }
-            if (team.getSize() >= 5) {
+            if (team.getSize() >= 3) {
                 p.sendMessage(ChatColor.RED + "This team is full!");
                 return;
             }
--- src/main/java/newamazingpvp/lifestealsmp/blacklistener/DisableMace.java
+++ src/main/java/newamazingpvp/lifestealsmp/blacklistener/DisableMace.java
@@ -1,5 +1,6 @@
 package newamazingpvp.lifestealsmp.blacklistener;
 
+import newamazingpvp.lifestealsmp.utility.DataBaseHelper;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -7,11 +8,47 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
 
+import java.sql.Timestamp;
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static newamazingpvp.lifestealsmp.events.TimeManager.formatDuration;
+import static newamazingpvp.lifestealsmp.events.TimeManager.isTimePassed;
+
 public class DisableMace implements Listener {
+
+    public static final DataBaseHelper maceDb;
+    static {
+        maceDb = new DataBaseHelper("maceCrafting.db");
+        maceDb.createTable("mace_crafting", "mace_id TEXT PRIMARY KEY, crafted_at TIMESTAMP");
+    }
+
     @EventHandler
     public void onCraftItem(CraftItemEvent event) {
         if (event.getCurrentItem() != null && event.getCurrentItem().getType() == Material.MACE) {
-            //         event.setCancelled(true);
+            String maceId = "MACE";
+            ZonedDateTime now = ZonedDateTime.now(ZoneId.systemDefault());
+            List<Map<String, Object>> results = maceDb.getData("mace_crafting", "mace_id = ?", maceId);
+            if (!results.isEmpty()) {
+                Map<String, Object> record = results.get(0);
+                Timestamp ts = (Timestamp) record.get("crafted_at");
+                ZonedDateTime lastCrafted = ts.toLocalDateTime().atZone(ZoneId.of("America/New_York"));
+                if (!isTimePassed(lastCrafted.plusDays(1))) {
+                    event.getWhoClicked().sendMessage(ChatColor.RED + "You must wait " + formatDuration(lastCrafted.plusDays(1)) + " before crafting mace (limited item) because it was recently crafted by someone.");
+                    event.setCancelled(true);
+                    return;
+                } else {
+                    Map<String, Object> updateValues = new HashMap<>();
+                    updateValues.put("crafted_at", Timestamp.valueOf(now.toLocalDateTime()));
+                    maceDb.updateData("mace_crafting", updateValues, "mace_id = ?", maceId);
+                }
+            } else {
+                maceDb.insertData("mace_crafting", new String[]{"mace_id", "crafted_at"}, maceId, Timestamp.valueOf(now.toLocalDateTime()));
+            }
+
             event.getWhoClicked().sendMessage(ChatColor.RED + "The mace is allowed but is nerfed in PVP for balance. Please refer to /rules for more information.");
             Player player = (Player) event.getView().getPlayer();
             player.sendTitle(ChatColor.RED + "WARNING!", "The mace is allowed but is nerfed in PVP for balance.");
