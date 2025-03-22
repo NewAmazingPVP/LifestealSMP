4d17485614c31505638c3a1ce66c00fd4adae285
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2025-03-22 18:04:29 -0400

    optimize code and render distance further to reduce lag
    optimize code and render distance further to reduce lag

--- src/main/java/newamazingpvp/lifestealsmp/listener/TotemLimiter.java
+++ src/main/java/newamazingpvp/lifestealsmp/listener/TotemLimiter.java
@@ -11,29 +11,30 @@ import static newamazingpvp.lifestealsmp.LifestealSMP.lifestealSmp;
 
 public class TotemLimiter {
 
+    private static final ItemStack TOTEM_ITEM = new ItemStack(Material.TOTEM_OF_UNDYING, 1);
+
     public static void totemLimiter() {
         new BukkitRunnable() {
             @Override
             public void run() {
                 for (Player player : Bukkit.getOnlinePlayers()) {
                     // this works but rlly ineffcient player.getInventory().all(Material.TOTEM_OF_UNDYING).size() > 5
-                    if (countTotems(player) > 5) {
+                    int totemCount = 0;
+                    for (ItemStack item : player.getInventory().getContents()) {
+                        if (item != null && item.getType() == Material.TOTEM_OF_UNDYING) {
+                            totemCount++;
+                            if (totemCount > 5) {
+                                break;
+                            }
+                        }
+                    }
+                    if (totemCount > 5) {
                         player.sendMessage(ChatColor.RED + "You have too many totems in your inventory! The totems have been dropped!!.");
-                        player.getInventory().removeItem(new ItemStack(Material.TOTEM_OF_UNDYING, 1));
-                        player.getWorld().dropItemNaturally(player.getLocation(), new ItemStack(Material.TOTEM_OF_UNDYING, 1));
+                        player.getInventory().removeItem(TOTEM_ITEM);
+                        player.getWorld().dropItemNaturally(player.getLocation(), TOTEM_ITEM);
                     }
                 }
             }
         }.runTaskTimer(lifestealSmp, 0L, 1L);
-    }
-
-    public static int countTotems(Player player) {
-        int count = 0;
-        for (ItemStack item : player.getInventory()) {
-            if (item != null && item.getType() == Material.TOTEM_OF_UNDYING) {
-                count++;
-            }
-        }
-        return count;
     }
 }
